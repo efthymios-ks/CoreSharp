1cbe5ea98ac25b6fe5c33fe59c69207dc2a84eac
Efthymios Koktsidis <efthymios.ks@gmail.com>
2021-04-11 13:24:05 +0300

    Added Exception extensions

---
+++ CoreSharp.Tests/Extensions/ExceptionExtensionsTests.cs
@@ -0,0 +1,75 @@
+﻿using NUnit.Framework;
+using CoreSharp.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace CoreSharp.Extensions.Tests
+{
+    [TestFixture]
+    public class ExceptionExtensionsTests
+    {
+        //Methods
+        [Test]
+        public void FlattenMessages_WhenExceptionNull_ThrowArgumentNullException()
+        {
+            //Arrange
+            Exception exception = null;
+
+            //Act
+            Action action = () => exception.FlattenMessages();
+
+            //Assert 
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        [Test]
+        public void FlattenMessages_WhenCalled_ReturnAllExceptionMessages()
+        {
+            //Arrange
+            var level3Exception = new Exception("Level 3");
+            var level2Exception = new AggregateException("Level 2", level3Exception);
+            var level1Exception = new Exception("Level 1", level2Exception);
+            var messages = new[] { level1Exception.Message, level2Exception.Message, level3Exception.Message };
+            var expected = string.Join(Environment.NewLine, messages);
+
+            //Act
+            var result = level1Exception.FlattenMessages();
+
+            //Assert 
+            result.Should().Be(expected);
+        }
+
+        [Test]
+        public void GetExceptions_WhenExceptionNull_ReturnEmptyEnumerable()
+        {
+            //Arrange
+            Exception exception = null;
+
+            //Act
+            Action action = () => exception.FlattenMessages();
+
+            //Assert 
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        [Test]
+        public void GetExceptions_WhenCalled_ReturnAllExceptionMessages()
+        {
+            //Arrange
+            var level3Exception = new Exception("Level 3");
+            var level2Exception = new AggregateException("Level 2", level3Exception);
+            var level1Exception = new Exception("Level 1", level2Exception);
+            var excepted = new[] { level1Exception, level2Exception, level3Exception };
+
+            //Act
+            var result = level1Exception.GetExceptions();
+
+            //Assert 
+            result.Should().Equal(excepted);
+        }
+    }
+}
---
+++ CoreSharp/Extensions/ExceptionExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CoreSharp.Extensions
+{
+    /// <summary>
+    /// Exception extensions.
+    /// </summary>
+    public static partial class ExceptionExtensions
+    {
+        /// <summary>
+        /// Get all messages including nested exceptions. 
+        /// </summary> 
+        public static string FlattenMessages(this Exception exception)
+        {
+            exception = exception ?? throw new ArgumentNullException(nameof(exception));
+
+            var exceptions = exception.GetExceptions();
+            var messages = exceptions
+                                .Where(e => !string.IsNullOrWhiteSpace(e.Message))
+                                .Select(e => e.Message.Trim());
+
+            string flattened = string.Join(Environment.NewLine, messages);
+            return flattened;
+        }
+
+        /// <summary>
+        /// Return list of exceptions including nested ones. 
+        /// </summary> 
+        public static IEnumerable<Exception> GetExceptions(this Exception exception)
+        {
+            exception = exception ?? throw new ArgumentNullException(nameof(exception));
+
+            return exception.GetExceptionsInternal();
+        }
+
+        public static IEnumerable<Exception> GetExceptionsInternal(this Exception exception)
+        {
+            yield return exception;
+
+            if (exception is AggregateException aggregateEx)
+            {
+                foreach (Exception innerEx in aggregateEx.InnerExceptions.SelectMany(e => e.GetExceptions()))
+                    yield return innerEx;
+            }
+            else if (exception.InnerException != null)
+            {
+                foreach (Exception innerEx in exception.InnerException.GetExceptions())
+                    yield return innerEx;
+            }
+        }
+    }
+}
