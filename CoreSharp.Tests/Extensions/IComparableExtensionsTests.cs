4c145f0fbe304cd1567c24e57e99b509551a5538
Efthymios Koktsidis <efthymios.ks@gmail.com>
2021-04-13 19:35:07 +0300

    Added IComparableExtensions

---
+++ CoreSharp.Tests/Extensions/IComparableExtensionsTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using FluentAssertions;
+
+namespace CoreSharp.Extensions.Tests
+{
+    [TestFixture]
+    public class IComparableExtensionsTests
+    {
+        [Test]
+        [TestCase(1, 2, 3, false, true)]
+        [TestCase(1, 0, 3, false, false)]
+        [TestCase(1, 4, 3, false, false)]
+        [TestCase(1, 1, 3, false, false)]
+        [TestCase(1, 1, 3, true, true)]
+        [TestCase(1, 3, 3, false, false)]
+        [TestCase(1, 3, 3, true, true)]
+        public void Between_ValueIsInRange_ReturnTrue(int left, int value, int right, bool includeEnds, bool expected)
+        {
+            //Act 
+            var result = value.Between(left, right, includeEnds);
+
+            //Assert
+            result.Should().Be(expected);
+        }
+    }
+}
---
+++ CoreSharp/Extensions/IComparableExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CoreSharp.Extensions
+{
+    /// <summary>
+    /// IComparable extensions. 
+    /// </summary>
+    public static partial class IComparableExtensions
+    {
+        /// <summary>
+        /// Check if an IComparable object is between two values. 
+        /// </summary>
+        public static bool Between<T>(this T value, T from, T to, bool includeEnds = true) where T : IComparable<T>
+        {
+            if (includeEnds)
+                return ((Comparer<T>.Default.Compare(value, from) >= 0) && (Comparer<T>.Default.Compare(value, to) <= 0));
+            else
+                return ((Comparer<T>.Default.Compare(value, from) > 0) && (Comparer<T>.Default.Compare(value, to) < 0));
+        }
+    }
+}
