97bd4dd086c8da5d0044631713fab01e8f9c69c3
Efthymios Koktsidis <efthymios.koktsidis@onelity.com>
2021-04-14 15:06:09 +0300

    Added IDictionary extensions

---
+++ CoreSharp.Tests/Extensions/IDictionaryExtensionsTests.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace CoreSharp.Extensions.Tests
+{
+    [TestFixture]
+    public class IDictionaryExtensionsTests
+    {
+        //Fields
+        private readonly IDictionary<string, int> dictionaryNull = null;
+        private readonly IDictionary<string, int> dictionary = new Dictionary<string, int>();
+
+        //Methods 
+        [SetUp]
+        public void SetUp()
+        {
+            dictionary.Clear();
+            dictionary.Add("1", 1);
+            dictionary.Add("2", 2);
+            dictionary.Add("3", 3);
+        }
+
+        [Test]
+        public void TryGet_SourceIsNull_ThrowArgumentNullException()
+        {
+            //Act 
+            Action action = () => dictionaryNull.TryGet("1", out _);
+
+            //Assert
+            action.Should().ThrowExactly<ArgumentNullException>();
+        }
+
+        [Test]
+        public void TryGet_KeyNotFound_ReturnFalse()
+        {
+            //Act 
+            var result = dictionary.TryGet("-1", out _);
+
+            //Assert
+            result.Should().BeFalse();
+        }
+
+        [Test]
+        public void TryGet_KeyFound_ReturnTrueAndValue()
+        {
+            //Act 
+            var result = dictionary.TryGet("1", out int value);
+
+            //Assert
+            result.Should().BeTrue();
+            value.Should().Be(1);
+        }
+
+        [Test]
+        public void TryAdd_SourceIsNull_ThrowArgumentNullException()
+        {
+            //Act 
+            Action action = () => dictionaryNull.TryAdd("1", 1);
+
+            //Assert
+            action.Should().ThrowExactly<ArgumentNullException>();
+        }
+
+        [Test]
+        public void TryAdd_KeyFound_ReturnFalseAndAddNothing()
+        {
+            //Arrange
+            var expected = new Dictionary<string, int>();
+            foreach (var entry in dictionary)
+                expected.Add(entry.Key, entry.Value);
+
+            //Act 
+            var added = dictionary.TryAdd("1", 1);
+
+            //Assert
+            added.Should().BeFalse();
+            dictionary.Should().Equal(expected);
+        }
+
+        [Test]
+        public void TryAdd_KeyNotFound_ReturnTrueAndAddValue()
+        {
+            //Arrange
+            var expected = new Dictionary<string, int>();
+            foreach (var entry in dictionary)
+                expected.Add(entry.Key, entry.Value);
+            expected.Add("4", 4);
+
+            //Act 
+            var added = dictionary.TryAdd("4", 4);
+
+            //Assert
+            added.Should().BeTrue();
+            dictionary.Should().Equal(expected);
+        }
+
+        [Test]
+        public void TryRemove_SourceIsNull_ThrowArgumentNullException()
+        {
+            //Act 
+            Action action = () => dictionaryNull.TryRemove("1", out _);
+
+            //Assert
+            action.Should().ThrowExactly<ArgumentNullException>();
+        }
+
+        [Test]
+        public void TryRemove_KeyNotFound_ReturnFalseAndRemoveNothing()
+        {
+            //Arrange
+            var expected = new Dictionary<string, int>();
+            foreach (var entry in dictionary)
+                expected.Add(entry.Key, entry.Value);
+
+            //Act 
+            var result = dictionary.TryRemove("-1", out _);
+
+            //Assert
+            result.Should().BeFalse();
+            dictionary.Should().Equal(expected);
+        }
+
+        [Test]
+        public void TryRemove_KeyFound_RemoveAndReturnValueAndTrue()
+        {
+            //Arrange
+            var expected = new Dictionary<string, int>();
+            foreach (var entry in dictionary)
+                expected.Add(entry.Key, entry.Value);
+            expected.Remove("3");
+
+            //Act 
+            var removed = dictionary.TryRemove("3", out int value);
+
+            //Assert
+            removed.Should().BeTrue();
+            value.Should().Be(3);
+            dictionary.Should().Equal(expected);
+        }
+
+        [Test]
+        public void TryUpdate_SourceIsNull_ThrowArgumentNullException()
+        {
+            //Act 
+            Action action = () => dictionaryNull.TryUpdate("-1", 100);
+
+            //Assert
+            action.Should().ThrowExactly<ArgumentNullException>();
+        }
+
+        [Test]
+        public void TryUpdate_UpdateActionIsNull_ThrowArgumentNullException()
+        {
+            //Arrange 
+            Func<string, int, int> updateAction = null;
+
+            //Act 
+            Action action = () => dictionary.TryUpdate("1", updateAction);
+
+            //Assert
+            action.Should().ThrowExactly<ArgumentNullException>();
+        }
+
+        [Test]
+        public void TryUpdate_KeyNotFound_ReturnFalse()
+        {
+            //Arrange
+            var expected = new Dictionary<string, int>();
+            foreach (var entry in dictionary)
+                expected.Add(entry.Key, entry.Value);
+
+            //Act 
+            var result = dictionary.TryUpdate("-1", 100);
+
+            //Assert
+            result.Should().BeFalse();
+            dictionary.Should().Equal(expected);
+        }
+
+        [Test]
+        public void TryUpdate_KeyFoundAndUpdateIsValue_ReturnTrueAndUpdateValue()
+        {
+            //Arrange
+            var expected = new Dictionary<string, int>();
+            foreach (var entry in dictionary)
+                expected.Add(entry.Key, entry.Value);
+            expected["1"] = 100;
+
+            //Act 
+            var result = dictionary.TryUpdate("1", 100);
+
+            //Assert
+            result.Should().BeTrue();
+            dictionary.Should().Equal(expected);
+        }
+
+        [Test]
+        public void TryUpdate_KeyFoundAndUpdateIsFunction_ReturnTrueAndUpdateValue()
+        {
+            //Arrange
+            var expected = new Dictionary<string, int>();
+            foreach (var entry in dictionary)
+                expected.Add(entry.Key, entry.Value);
+            static int updateAction(string key, int value) => value = value * 2;
+            expected["1"] = updateAction("1", 1);
+
+            //Act 
+            var result = dictionary.TryUpdate("1", updateAction);
+
+            //Assert
+            result.Should().BeTrue();
+            dictionary.Should().Equal(expected);
+        }
+    }
+}
---
+++ CoreSharp/Extensions/IDictionaryExtensions.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CoreSharp.Extensions
+{
+    /// <summary>
+    /// IDictionary extensions. 
+    /// </summary>
+    public static class IDictionaryExtensions
+    {
+        /// <summary>
+        /// Attempts to get the item with the specifed key. 
+        /// </summary> 
+        public static bool TryGet<T>(this IDictionary<string, T> source, string key, out T value)
+        {
+            source = source ?? throw new ArgumentNullException(nameof(source));
+
+            return source.TryGetValue(key, out value);
+        }
+
+        /// <summary>
+        /// Attempts to add the item with the specifed key. 
+        /// </summary> 
+        public static bool TryAdd<T>(this IDictionary<string, T> source, string key, T value)
+        {
+            source = source ?? throw new ArgumentNullException(nameof(source));
+
+            if (!source.ContainsKey(key))
+            {
+                source.Add(key, value);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Attempts to remove the item the specified key in dictionary. 
+        /// </summary> 
+        public static bool TryRemove<T>(this IDictionary<string, T> source, string key)
+        {
+            source = source ?? throw new ArgumentNullException(nameof(source));
+
+            return source.TryRemove(key, out _);
+        }
+
+        /// <summary>
+        /// Attempts to remove the item the specified key in dictionary and return the value removed. 
+        /// </summary> 
+        public static bool TryRemove<T>(this IDictionary<string, T> source, string key, out T value)
+        {
+            source = source ?? throw new ArgumentNullException(nameof(source));
+
+            value = default;
+            if (source.ContainsKey(key))
+            {
+                value = source[key];
+                source.Remove(key);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Attempts to update the specifed key in dictionary, if exists. 
+        /// </summary> 
+        public static bool TryUpdate<T>(this IDictionary<string, T> source, string key, T value)
+        {
+            source = source ?? throw new ArgumentNullException(nameof(source));
+
+            return source.TryUpdate<T>(key, (k, v) => value);
+        }
+
+        /// <summary>
+        /// Attempts to update the specifed key in dictionary, if exists. 
+        /// </summary> 
+        public static bool TryUpdate<T>(this IDictionary<string, T> source, string key, Func<string, T, T> updateAction)
+        {
+            source = source ?? throw new ArgumentNullException(nameof(source));
+            updateAction = updateAction ?? throw new ArgumentNullException(nameof(updateAction));
+
+            if (source.ContainsKey(key))
+            {
+                var newValue = updateAction(key, source[key]);
+                source[key] = newValue;
+                return true;
+            }
+            else
+                return false;
+        }
+
+        /// <summary> 
+        /// Attempts to add or update an item with the specified key. 
+        /// </summary> 
+        /// <returns>True if added, false if updated</returns>
+        public static T AddOrUpdate<T>(this IDictionary<string, T> source, string key, T value)
+        {
+            source = source ?? throw new ArgumentNullException(nameof(source));
+
+            return source.AddOrUpdate(key, value, (k, v) => value);
+        }
+
+        /// <summary> 
+        /// Attempts to add or update an item with the specified key. 
+        /// </summary> 
+        /// <returns>True if added, false if updated</returns>
+        public static T AddOrUpdate<T>(this IDictionary<string, T> source, string key, T addValue, T updateValue)
+        {
+            source = source ?? throw new ArgumentNullException(nameof(source));
+
+            return source.AddOrUpdate(key, addValue, (k, v) => updateValue);
+        }
+
+        /// <summary> 
+        /// Attempts to add or update an item with the specified key. 
+        /// </summary> 
+        public static T AddOrUpdate<T>(this IDictionary<string, T> source, string key, T addValue, Func<string, T, T> updateAction)
+        {
+            source = source ?? throw new ArgumentNullException(nameof(source));
+            updateAction = updateAction ?? throw new ArgumentNullException(nameof(updateAction));
+
+            if (source.ContainsKey(key))
+                source.TryUpdate(key, updateAction);
+            else
+                source.TryAdd(key, addValue);
+
+            return source[key];
+        }
+
+        /// <summary> 
+        /// If value exists, get, else add and get.  
+        /// </summary> 
+        /// <returns>Value found or added.</returns>
+        public static T GetOrAdd<T>(this IDictionary<string, T> source, string key, T addValue)
+        {
+            source = source ?? throw new ArgumentNullException(nameof(source));
+
+            source.TryAdd(key, addValue);
+            return source[key];
+        }
+
+        /// <summary>
+        /// Converts dictionary to KeyValuePair enumerable. 
+        /// </summary> 
+        public static IEnumerable<KeyValuePair<string, T>> ToEnumerable<T>(this IDictionary<string, T> source)
+        {
+            source = source ?? throw new ArgumentNullException(nameof(source));
+
+            return source.Select(i => new KeyValuePair<string, T>(i.Key, i.Value));
+        }
+
+        /// <summary>
+        /// Returns given value occurences in dictionary.
+        /// </summary>
+        public static int ContainsValue<T>(this IDictionary<string, T> source, T value)
+        {
+            source = source ?? throw new ArgumentNullException(nameof(source));
+
+            return source.Count(i => i.Value.Equals(value));
+        }
+    }
+}
