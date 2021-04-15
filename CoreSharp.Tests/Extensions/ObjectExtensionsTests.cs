5a0f64c9ee550adebd73edd54a83a40e8df52dff
Efthymios Koktsidis <efthymios.ks@gmail.com>
2021-04-15 20:38:58 +0300

    Added Object extensions

---
+++ CoreSharp.Tests/Extensions/ObjectExtensionsTests.cs
@@ -0,0 +1,171 @@
+﻿using NUnit.Framework;
+using CoreSharp.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace CoreSharp.Extensions.Tests
+{
+    [TestFixture()]
+    public class ObjectExtensionsTests
+    {
+        [Test]
+        public void Is_InputIsNull_ReturnFalse()
+        {
+            //Arrange
+            object input = null;
+
+            //Action
+            var result = input.Is<string>();
+
+            //Assert
+            result.Should().BeFalse();
+        }
+
+        [Test]
+        public void Is_InputTypeDiffers_ReturnFalse()
+        {
+            //Arrange
+            object input = 1;
+
+            //Action
+            var result = input.Is<string>();
+
+            //Assert
+            result.Should().BeFalse();
+        }
+
+        [Test]
+        public void Is_InputTypeMatches_ReturnTrue()
+        {
+            //Arrange
+            object input = 1;
+
+            //Action
+            var result = input.Is<int>();
+
+            //Assert
+            result.Should().BeTrue();
+        }
+
+        [Test]
+        public void As_InputIsNull_ReturnNull()
+        {
+            //Arrange
+            object input = null;
+
+            //Action
+            var result = input.As<string>();
+
+            //Assert
+            result.Should().BeNull();
+        }
+
+        [Test]
+        public void As_InputTypeDiffers_ReturnNull()
+        {
+            //Arrange
+            object input = 1;
+
+            //Action
+            var result = input.As<string>();
+
+            //Assert
+            result.Should().BeNull();
+        }
+
+        [Test]
+        public void As_InputTypeMatches_ReturnCastValue()
+        {
+            //Arrange
+            object input = 1;
+
+            //Action
+            var result = input.As<int>();
+
+            //Assert
+            result.Should().Be(1);
+        }
+
+        [Test]
+        public void TryCast_InputIsNull_ReturnDefault()
+        {
+            //Arrange
+            object input = null;
+
+            //Action
+            var result = input.TryCast<int>();
+
+            //Assert
+            result.Should().Be(default);
+        }
+
+        [Test]
+        public void TryCast_InputIsDbNull_ReturnDefault()
+        {
+            //Arrange
+            object input = DBNull.Value;
+
+            //Action
+            var result = input.TryCast<int>();
+
+            //Assert
+            result.Should().Be(default);
+        }
+
+        [Test]
+        public void TryCast_InputTypeDiffers_ReturnDefault()
+        {
+            //Arrange
+            object input = "1";
+
+            //Action
+            var result = input.TryCast<int>();
+
+            //Assert
+            result.Should().Be(default);
+        }
+
+        [Test]
+        public void TryCast_InputMissMatchAndGenericIsString_ReturnStringEmpty()
+        {
+            //Arrange
+            object input = DBNull.Value;
+
+            //Action
+            var result = input.TryCast<string>();
+
+            //Assert
+            result.Should().Be(string.Empty);
+        }
+
+        [Test]
+        public void TryCast_InputMissMatchAndGivenDefaultValue_ReturnGivenDefaultValue()
+        {
+            //Arrange
+            object input = "1";
+
+            //Action
+            var result = input.TryCast(2);
+
+            //Assert
+            result.Should().Be(2);
+        }
+
+        [Test]
+        public void TryCast_InputTypeMatches_ReturnCastValue()
+        {
+            //Arrange
+            object input = 1;
+
+            //Action
+            var result = input.TryCast<int>();
+
+            //Assert
+            result.Should().Be(1);
+        }
+    }
+}
---
+++ CoreSharp/Extensions/ObjectExtensions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace CoreSharp.Extensions
+{
+    /// <summary>
+    /// Object extensions. 
+    /// </summary>
+    public static partial class ObjectExtensions
+    {
+        /// <summary>
+        /// Check if the runtime type of an expression is 
+        /// compatible with a given type. 
+        /// </summary>
+        public static bool Is<T>(this object input)
+        {
+            return input is T;
+        }
+
+        /// <summary>
+        /// Explicitly convert an expression to a given type 
+        /// if its runtime type is compatible with that type. 
+        /// </summary>
+        public static T As<T>(this object input) where T : class
+        {
+            return input as T;
+        }
+
+        /// <summary>
+        /// Try casting input to given type and 
+        /// return default if null or of different type. 
+        /// </summary>
+        public static T TryCast<T>(this object input)
+        {
+            T defaultValue = default;
+            if (typeof(T) == typeof(string))
+                defaultValue = (T)(object)string.Empty;
+            return input.TryCast(defaultValue);
+        }
+
+        /// <summary>
+        /// Try casting input to given type and 
+        /// return default if null or of different type. 
+        /// </summary>
+        public static T TryCast<T>(this object input, T defaultValue)
+        {
+            if (input == null)
+                return defaultValue;
+            else if (DBNull.Value.Equals(input))
+                return defaultValue;
+            else if (input.GetType() != typeof(T))
+                return defaultValue;
+            else if (!(input is T))
+                return defaultValue;
+            else
+                return (T)input;
+        }
+    }
+}
