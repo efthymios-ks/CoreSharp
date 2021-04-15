8fb240f167dec043881bf5d876f1ac348bdc0ae5
Efthymios Koktsidis <efthymios.koktsidis@onelity.com>
2021-04-15 16:30:03 +0300

    Added IList extensions

---
+++ CoreSharp.Tests/Extensions/IListExtensionsTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using FluentAssertions;
+
+namespace CoreSharp.Extensions.Tests
+{
+    [TestFixture]
+    public class IListExtensionsTests
+    {
+        //Fields
+        private readonly IList<int> sourceNull = null;
+
+        //Methods 
+        [Test]
+        public void Fill_SourceIsNull_ThrowArgumentNullException()
+        {
+            //Act
+            Action action = () => sourceNull.Fill(0);
+
+            //Assert
+            action.Should().ThrowExactly<ArgumentNullException>();
+        }
+
+        [Test]
+        public void Fill_WhenCalled_FillListWithSameValue()
+        {
+            //Arrange
+            IList<int> source = new List<int>() { 1, 2, 3 };
+            IList<int> expected = new List<int> { 0, 0, 0 };
+
+            //Act
+            source.Fill(0);
+
+            //Assert
+            source.Should().Equal(expected);
+        }
+    }
+}
---
+++ CoreSharp/Extensions/IListExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CoreSharp.Extensions
+{
+    /// <summary>
+    /// IList extensions. 
+    /// </summary>
+    public static partial class IListExtensions
+    {
+        public static void Fill<T>(this IList<T> source, T value)
+        {
+            source = source ?? throw new ArgumentNullException(nameof(source));
+
+            for (int i = 0; i < source.Count; i++)
+                source[i] = value;
+        }
+    }
+}
