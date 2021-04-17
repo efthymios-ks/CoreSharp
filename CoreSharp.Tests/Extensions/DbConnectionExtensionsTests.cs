d724b8fc3a48aa5d38154e911aeb3d5b9524f6a0
Efthymios Koktsidis <efthymios.ks@gmail.com>
2021-04-17 21:16:18 +0300

    Added DbConnection extensions

---
+++ CoreSharp.Tests/Extensions/DbConnectionExtensionsTests.cs
@@ -0,0 +1,106 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Data.Common;
+using System.Data.SqlClient;
+
+namespace CoreSharp.Extensions.Tests
+{
+    [TestFixture]
+    public class DbConnectionExtensionsTests
+    {
+        //Fields
+        private readonly DbConnection connectionNull = null;
+        private DbConnection sqlConnection;
+
+        //Methods
+        [SetUp]
+        public void SetUp()
+        {
+            sqlConnection = new SqlConnection();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            sqlConnection?.Dispose();
+            sqlConnection = null;
+        }
+
+        [Test]
+        public void CreateDataAdapter_ConnectionIsNull_ThrowArgumentNullException()
+        {
+            //Act
+            Action action = () => connectionNull.CreateDataAdapter();
+
+            //Assert
+            action.Should().ThrowExactly<ArgumentNullException>();
+        }
+
+        [Test]
+        public void CreateDataAdapter_WhenCalled_ReturnSameTypeDbAdapter()
+        {
+            //Act
+            var result = sqlConnection.CreateDataAdapter();
+
+            //Assert
+            result.Should().BeOfType<SqlDataAdapter>();
+        }
+
+        [Test]
+        public void CreateParameter_ConnectionIsNull_ThrowArgumentNullException()
+        {
+            //Act
+            Action action = () => connectionNull.CreateParameter("{name}", "Efthymios");
+
+            //Assert
+            action.Should().ThrowExactly<ArgumentNullException>();
+        }
+
+        [Test]
+        public void CreateParameter_WhenCalled_ReturnSameTypeDbParameterWithValues()
+        {
+            //Arrange
+            string name = "{name}";
+            string value = "Efthymios";
+
+            //Act
+            var result = sqlConnection.CreateParameter(name, value);
+
+            //Assert
+            result.Should().BeOfType<SqlParameter>();
+            result.ParameterName.Should().Be(name);
+            result.Value.Should().Be(value);
+        }
+
+        [Test]
+        public void OpenTransaction_ConnectionIsNull_ThrowArgumentNullException()
+        {
+            //Act
+            Action action = () => connectionNull.OpenTransaction();
+
+            //Assert
+            action.Should().ThrowExactly<ArgumentNullException>();
+        }
+
+        [Test]
+        public void IsOpen_ConnectionIsNull_ThrowArgumentNullException()
+        {
+            //Act
+            Action action = () => connectionNull.IsOpen();
+
+            //Assert
+            action.Should().ThrowExactly<ArgumentNullException>();
+        }
+
+        [Test]
+        public void IsAvailable_ConnectionIsNull_ThrowArgumentNullException()
+        {
+            //Act
+            Action action = () => connectionNull.IsOpen();
+
+            //Assert
+            action.Should().ThrowExactly<ArgumentNullException>();
+        }
+    }
+}
---
+++ CoreSharp/Extensions/DbConnectionExtensions.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Data;
+using System.Data.Common;
+using System.Threading.Tasks;
+
+namespace CoreSharp.Extensions
+{
+    //TODO: Finish unit tests for DbConnection. 
+    /// <summary>
+    /// DbConnection extensions. 
+    /// </summary>
+    public static partial class DbConnectionExtensions
+    {
+        /// <summary>
+        /// Return a new instance of the provider's class that implements the DbDataAdapter class.
+        /// </summary>
+        public static DbDataAdapter CreateDataAdapter(this DbConnection connection)
+        {
+            connection = connection ?? throw new ArgumentNullException(nameof(connection));
+
+            var factory = DbProviderFactories.GetFactory(connection);
+            var adapter = factory.CreateDataAdapter();
+            return adapter;
+        }
+
+        /// <summary>
+        /// Return a new instance of the provider's class that implements the DbParameter class. 
+        /// </summary>
+        public static DbParameter CreateParameter(this DbConnection connection, string parameterName, object parameterValue)
+        {
+            connection = connection ?? throw new ArgumentNullException(nameof(connection));
+
+            var factory = DbProviderFactories.GetFactory(connection);
+            var parameter = factory.CreateParameter(parameterName, parameterValue);
+            return parameter;
+        }
+
+        /// <summary>
+        /// Try to open the connection and begin a new transaction on it. 
+        /// </summary>
+        public static DbTransaction OpenTransaction(this DbConnection connection)
+        {
+            return connection.OpenTransaction(IsolationLevel.ReadCommitted);
+        }
+
+        /// <summary>
+        /// Try to open the connection and begin a new transaction on it. 
+        /// </summary>
+        public static DbTransaction OpenTransaction(this DbConnection connection, IsolationLevel isolationLevel)
+        {
+            return connection
+                .OpenTransactionAsync(isolationLevel)
+                .GetAwaiter()
+                .GetResult();
+        }
+
+        /// <summary>
+        /// Try to open the connection and begin a new transaction on it. 
+        /// </summary>
+        public static async Task<DbTransaction> OpenTransactionAsync(this DbConnection connection)
+        {
+            return await connection.OpenTransactionAsync(IsolationLevel.ReadCommitted);
+        }
+
+        /// <summary>
+        /// Try to open the connection and begin a new transaction on it. 
+        /// </summary>
+        public static async Task<DbTransaction> OpenTransactionAsync(this DbConnection connection, IsolationLevel isolationLevel)
+        {
+            connection = connection ?? throw new ArgumentNullException(nameof(connection));
+
+            if (!connection.IsOpen())
+                await connection.OpenAsync();
+
+            return connection.BeginTransaction(isolationLevel);
+        }
+
+        /// <summary>
+        /// Check if connection is open. 
+        /// </summary>
+        public static bool IsOpen(this DbConnection connection)
+        {
+            connection = connection ?? throw new ArgumentNullException(nameof(connection));
+
+            return connection.State.HasFlag(ConnectionState.Open);
+        }
+
+        /// <summary>
+        /// Check if connection is available. 
+        /// </summary>
+        public static bool IsAvailable(this DbConnection connection)
+        {
+            connection = connection ?? throw new ArgumentNullException(nameof(connection));
+
+            return connection
+                    .IsAvailableAsync()
+                    .GetAwaiter()
+                    .GetResult();
+        }
+
+        /// <summary>
+        /// Check if connection is available. 
+        /// </summary>
+        public static async Task<bool> IsAvailableAsync(this DbConnection connection)
+        {
+            connection = connection ?? throw new ArgumentNullException(nameof(connection));
+
+            try
+            {
+                await connection.OpenAsync();
+                connection.Close();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
