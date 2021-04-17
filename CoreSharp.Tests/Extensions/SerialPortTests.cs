e762b63da60476d6cc54f4b7a752e5e3dc8d9a69
Efthymios Koktsidis <efthymios.ks@gmail.com>
2021-04-17 13:04:02 +0300

    Added SerialPort extensions

---
+++ CoreSharp.Tests/Extensions/SerialPortTests.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using System;
+using System.Text;
+using System.IO.Ports;
+using FluentAssertions;
+
+namespace CoreSharp.Extensions.Tests
+{
+    [TestFixture]
+    public class SerialPortTests
+    {
+        [Test]
+        public void GetSettings_PortIsNull_ThrowArgumentNullException()
+        {
+            //Arrange 
+            SerialPort port = null;
+
+            //Act
+            Action action = () => port.GetSettings();
+
+            //Assert
+            action.Should().ThrowExactly<ArgumentNullException>();
+        }
+
+        [Test]
+        public void GetSettings_WhenCalled_ReturnMatchingSettings()
+        {
+            //Arrange 
+            var port = new SerialPort("COM1", 4800, Parity.Even, 7, StopBits.One)
+            {
+                Encoding = Encoding.UTF8,
+                ReadTimeout = TimeSpan.FromMilliseconds(500).Milliseconds
+            };
+
+            //Act
+            var result = port.GetSettings();
+
+            //Assert
+            result.PortName.Should().Be(port.PortName);
+            result.BaudRate.Should().Be(port.BaudRate);
+            result.Parity.Should().Be(port.Parity);
+            result.DataBits.Should().Be(port.DataBits);
+            result.StopBits.Should().Be(port.StopBits);
+            result.TextEncoding.CodePage.Should().Be(port.Encoding.CodePage);
+            result.Timeout.Milliseconds.Should().BeOneOf(port.ReadTimeout, port.WriteTimeout);
+        }
+    }
+}
---
+++ CoreSharp/Extensions/SerialPortExtensions.cs
@@ -0,0 +1,32 @@
+﻿using CoreSharp.Models;
+using System;
+using System.IO.Ports;
+
+namespace CoreSharp.Extensions
+{
+    /// <summary>
+    /// SerialPort extensions. 
+    /// </summary>
+    public static partial class SerialPortExtensions
+    {
+        /// <summary>
+        /// Extract SerialPortSettings from SerialPort. 
+        /// </summary>
+        public static SerialPortSettings GetSettings(this SerialPort port)
+        {
+            port = port ?? throw new ArgumentNullException();
+
+
+            return new SerialPortSettings()
+            {
+                PortName = port.PortName,
+                BaudRate = port.BaudRate,
+                Parity = port.Parity,
+                DataBits = port.DataBits,
+                StopBits = port.StopBits,
+                TextEncoding = port.Encoding,
+                Timeout = TimeSpan.FromMilliseconds(Math.Min(port.ReadTimeout, port.WriteTimeout))
+            };
+        }
+    }
+}
