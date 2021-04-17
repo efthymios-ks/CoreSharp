de016a4f8f8c700dfbdcc0ba017e1e10959fcc9d
Efthymios Koktsidis <efthymios.ks@gmail.com>
2021-04-17 21:16:09 +0300

    Added DbCommand extensions

---
+++ CoreSharp.Tests/Extensions/DbCommandExtensionsTests.cs
@@ -0,0 +1,83 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Data.Common;
+using System.Data.SqlClient;
+
+namespace CoreSharp.Extensions.Tests
+{
+    [TestFixture]
+    public class DbCommandExtensionsTests
+    {
+        //Fields
+        private readonly DbCommand commandNull = null;
+        private SqlCommand sqlCommand;
+
+        //Methods
+        [SetUp]
+        public void SetUp()
+        {
+            sqlCommand = new SqlCommand();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            sqlCommand?.Dispose();
+            sqlCommand = null;
+        }
+
+        [Test]
+        public void CreateParameter_CommandIsNull_ThrowArgumentNullException()
+        {
+            //Act 
+            Action action = () => commandNull.CreateParameter("{name}", "Efthymios");
+
+            //Assert
+            action.Should().ThrowExactly<ArgumentNullException>();
+        }
+
+        [Test]
+        public void CreateParameter_WhenCalled_ReturnSameTypeDbParameterWithValues()
+        {
+            //Arrang 
+            string name = "{name}";
+            string value = "Efthymios";
+
+            //Act 
+            var result = sqlCommand.CreateParameter(name, value);
+
+            //Assert
+            result.Should().BeOfType<SqlParameter>();
+            result.ParameterName.Should().Be(name);
+            result.Value.Should().Be(value);
+        }
+
+        [Test]
+        public void AddParameter_CommandIsNull_ThrowArgumentNullException()
+        {
+            //Act 
+            Action action = () => commandNull.AddParameterWithValue("{name}", "Efthymios");
+
+            //Assert
+            action.Should().ThrowExactly<ArgumentNullException>();
+        }
+
+        [Test]
+        public void AddParameter_WhenCalled_AddsAndReturnSameTypeDbParameterWithValues()
+        {
+            //Arrang 
+            string name = "{name}";
+            string value = "Efthymios";
+
+            //Act 
+            var result = sqlCommand.AddParameterWithValue(name, value);
+
+            //Assert
+            result.Should().BeOfType<SqlParameter>();
+            result.ParameterName.Should().Be(name);
+            result.Value.Should().Be(value);
+            sqlCommand.Parameters.Should().Contain(result);
+        }
+    }
+}
---
+++ CoreSharp/Extensions/DbCommandExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data.Common;
+
+namespace CoreSharp.Extensions
+{
+    /// <summary>
+    /// DbCommand extensions. 
+    /// </summary>
+    public static partial class DbCommandExtensions
+    {
+        /// <summary>
+        /// Return a new instance of the provider's class that implements the DbParameter class. 
+        /// </summary>
+        public static DbParameter CreateParameter(this DbCommand command, string name, object value)
+        {
+            command = command ?? throw new ArgumentNullException(nameof(command));
+
+            var parameter = command.CreateParameter();
+            parameter.ParameterName = name;
+            parameter.Value = value;
+            return parameter;
+        }
+
+        /// <summary>
+        /// Adds the specified DbParameter object to the DbParameterCollection. 
+        /// </summary>
+        public static DbParameter AddParameterWithValue(this DbCommand command, string name, object value)
+        {
+            command = command ?? throw new ArgumentNullException(nameof(command));
+
+            var parameter = command.CreateParameter(name, value);
+            command.Parameters.Add(parameter);
+            return parameter;
+        }
+    }
+}
