f92e432ed40802789d9f80a2f5652252d33c36ae
Efthymios Koktsidis <efthymios.ks@gmail.com>
2021-04-17 21:16:29 +0300

    Added DbProviderFactory extensions

---
+++ CoreSharp.Tests/Extensions/DbProviderFactoryExtenionsTests.cs
@@ -0,0 +1,51 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Data.Common;
+using System.Data.SqlClient;
+
+namespace CoreSharp.Extensions.Tests
+{
+    [TestFixture]
+    public class DbProviderFactoryExtenionsTests
+    {
+        //Fields
+        private const string SqlProviderName = "Microsoft.Data.SqlClient";
+        private readonly DbProviderFactory factoryNull = null;
+        private DbProviderFactory sqlFactory;
+
+        //Methods 
+        [SetUp]
+        public void SetUp()
+        {
+            using (var connection = new SqlConnection())
+                sqlFactory = DbProviderFactories.GetFactory(connection);
+        }
+
+        [Test]
+        public void CreateParameter_FactoryIsNull_ThrowArgumentNullException()
+        {
+            //Act
+            Action action = () => factoryNull.CreateParameter("{name}", "Efthymios");
+
+            //Assert
+            action.Should().ThrowExactly<ArgumentNullException>();
+        }
+
+        [Test]
+        public void CreateParameter_WhenCalled_ReturnSameTypeDbParameterWithValues()
+        {
+            //Arrange
+            string name = "{name}";
+            string value = "Efthymios";
+
+            //Act
+            var result = sqlFactory.CreateParameter(name, value);
+
+            //Assert
+            result.Should().BeOfType<SqlParameter>();
+            result.ParameterName.Should().Be(name);
+            result.Value.Should().Be(value);
+        }
+    }
+}
---
+++ CoreSharp/Extensions/DbProviderFactoryExtenions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data.Common;
+
+namespace CoreSharp.Extensions
+{
+    /// <summary>
+    /// DbProviderFactory extenions
+    /// </summary>
+    public static partial class DbProviderFactoryExtenions
+    {
+        /// <summary>
+        /// Return a new instance of the provider's class that implements the DbParameter class. 
+        /// </summary>
+        public static DbParameter CreateParameter(this DbProviderFactory factory, string parameterName, object parameterValue)
+        {
+            factory = factory ?? throw new ArgumentNullException(nameof(factory));
+
+            var parameter = factory.CreateParameter();
+            parameter.ParameterName = parameterName;
+            parameter.Value = parameterValue;
+            return parameter;
+        }
+    }
+}
