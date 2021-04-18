39116c6e95dcb8dccc6636511325ad179355b8a0
Efthymios Koktsidis <efthymios.ks@gmail.com>
2021-04-18 12:30:49 +0300

    Added StringBuilder extensions

---
+++ CoreSharp.Tests/Extensions/StringBuilderExtensionsTests.cs
@@ -0,0 +1,70 @@
+﻿using NUnit.Framework;
+using CoreSharp.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using System.Globalization;
+
+namespace CoreSharp.Extensions.Tests
+{
+    [TestFixture]
+    public class StringBuilderExtensionsTests
+    {
+        //Fields
+        private readonly StringBuilder builderNull = null;
+        private readonly StringBuilder builder = new StringBuilder();
+
+        //Methods
+        [SetUp]
+        public void SetUp()
+        {
+            builder.Clear();
+        }
+
+        [Test]
+        public void AppendFormatLine_BuilderIsNull_ThrowArgumentNullException()
+        {
+            //Act
+            Action action = () => builderNull.AppendFormatLine("{0}", 1);
+
+            //Assert
+            action.Should().ThrowExactly<ArgumentNullException>();
+        }
+
+        [Test]
+        public void AppendFormatLine_WhenCalled_AppendFormattedStringAndNewLine()
+        {
+            //Arrange
+            string format = "{0}";
+            int value = 1000;
+            var culture = CultureInfo.CurrentCulture;
+            string formatResult = string.Format(culture, format, value);
+            string expected = formatResult + Environment.NewLine;
+
+            //Act
+            var result = builder.AppendFormatLine(culture, format, value);
+
+            //Assert
+            result.ToString().Should().EndWith(expected);
+        }
+
+        [Test]
+        public void AppendFormatLineCI_WhenCalledAppendStringFormatWithInvariantCultureArgument()
+        {
+            //Arrange
+            string format = "{0}";
+            int value = 1000;
+            string formatResult = string.Format(CultureInfo.InvariantCulture, format, value);
+            string expected = formatResult + Environment.NewLine;
+
+            //Act
+            var result = builder.AppendFormatLineCI(format, value);
+
+            //Assert
+            result.ToString().Should().EndWith(expected);
+        }
+    }
+}
---
+++ CoreSharp/Extensions/StringBuilderExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+
+namespace CoreSharp.Extensions
+{
+    /// <summary>
+    /// StringBuilder extensions.
+    /// </summary>
+    public static partial class StringBuilderExtensions
+    {
+        /// <summary>
+        /// Append String.Format + NewLine.  
+        /// </summary>
+        public static StringBuilder AppendFormatLine(this StringBuilder builder, string format, params object[] parameters)
+        {
+            return builder.AppendFormatLine(CultureInfo.CurrentCulture, format, parameters);
+        }
+
+        /// <summary>
+        /// Append InvariantCulture String.Format + NewLine.  
+        /// </summary>
+        public static StringBuilder AppendFormatLineCI(this StringBuilder builder, string format, params object[] parameters)
+        {
+            return builder.AppendFormatLine(CultureInfo.InvariantCulture, format, parameters);
+        }
+
+        /// <summary>
+        /// Append StringFormat with custom formatProvider + NewLine. 
+        /// </summary>
+        public static StringBuilder AppendFormatLine(this StringBuilder builder, IFormatProvider formatProvider, string format, params object[] parameters)
+        {
+            builder = builder ?? throw new ArgumentNullException(nameof(builder));
+            formatProvider = formatProvider ?? throw new ArgumentNullException(nameof(formatProvider));
+            parameters = parameters ?? throw new ArgumentNullException(nameof(parameters));
+
+            builder.AppendFormat(formatProvider, format, parameters);
+            builder.AppendLine();
+            return builder;
+        }
+    }
+}
