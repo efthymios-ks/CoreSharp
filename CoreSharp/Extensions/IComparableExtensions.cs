f86f72b967e9559ab239f70d1610b15a6456037b
Efthymios Koktsidis <efthymios.koktsidis@onelity.com>
2021-04-20 09:20:02 +0300

    Remove unnecessary code

--- CoreSharp/Extensions/IComparableExtensions.cs
+++ CoreSharp/Extensions/IComparableExtensions.cs
@@ -14,9 +14,9 @@ namespace CoreSharp.Extensions
         public static bool Between<T>(this T value, T from, T to, bool includeEnds = true) where T : IComparable<T>
         {
             if (includeEnds)
-                return ((Comparer<T>.Default.Compare(value, from) >= 0) && (Comparer<T>.Default.Compare(value, to) <= 0));
+                return (Comparer<T>.Default.Compare(value, from) >= 0) && (Comparer<T>.Default.Compare(value, to) <= 0);
             else
-                return ((Comparer<T>.Default.Compare(value, from) > 0) && (Comparer<T>.Default.Compare(value, to) < 0));
+                return (Comparer<T>.Default.Compare(value, from) > 0) && (Comparer<T>.Default.Compare(value, to) < 0);
         }
     }
 }
