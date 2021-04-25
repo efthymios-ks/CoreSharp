bc39e478260aad6defa41a82b556ab13ea7ab188
Efthymios Koktsidis <efthymios.ks@gmail.com>
2021-04-25 11:53:01 +0300

    Added INotifyPropertyChanged abstract implementation

---
+++ CoreSharp/Implementations/ObservableObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace CoreSharp.Implementations
+{
+    /// <summary>
+    /// Base class implementing INotifyPropertyChanged. 
+    /// </summary>
+    public abstract class ObservableObject : INotifyPropertyChanged
+    {
+        //Events  
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        //Methods 
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var args = new PropertyChangedEventArgs(propertyName);
+            PropertyChanged?.Invoke(this, args);
+        }
+
+        protected void SetProperty<T>(ref T backingField, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(backingField, newValue))
+                return;
+
+            backingField = newValue;
+            OnPropertyChanged(propertyName);
+        }
+    }
+}
