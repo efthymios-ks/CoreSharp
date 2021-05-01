2cd7acb69e99c9bb42db83d1cae224ebeb981a04
Efthymios Koktsidis <efthymios.ks@gmail.com>
2021-05-02 00:35:53 +0300

    Added Array.GetRow and Array.GetColumn extensions

---
+++ CoreSharp.Tests/Extensions/ArrayExtensionsTests.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace CoreSharp.Extensions.Tests
+{
+    [TestFixture]
+    public class ArrayExtensionsTests
+    {
+        //Fields
+        private readonly int[,] sourceNull = null;
+
+        [Test]
+        public void GetRow_SourceIsNull_ThrowArgumentNullException()
+        {
+            //Assert
+            Action action = () => sourceNull.GetRow(0);
+
+            //Assert
+            action.Should().ThrowExactly<ArgumentNullException>();
+        }
+
+        [Test]
+        [TestCase(0, 0, -1)]
+        [TestCase(0, 0, 1)]
+        public void GetRow_RowIndexInvalid_ThrowArgumentOutOfRangeException(int rows, int columns, int row)
+        {
+            //Arrange
+            var source = new int[rows, columns];
+
+            //Assert
+            Action action = () => source.GetRow(row);
+
+            //Assert
+            action.Should().ThrowExactly<ArgumentOutOfRangeException>();
+        }
+
+        [Test]
+        public void GetRow_WhenCalled_ReturnRow()
+        {
+            //Arrange
+            var source = new int[2, 2]
+            {
+                { 1, 2 },
+                { 3, 4 }
+            };
+            var expected = new[] { 3, 4 };
+
+            //Assert
+            var result = source.GetRow(1);
+
+            //Assert
+            result.Should().Equal(expected);
+        }
+
+        [Test]
+        public void GetColumn_SourceIsNull_ThrowArgumentNullException()
+        {
+            //Assert
+            Action action = () => sourceNull.GetColumn(0);
+
+            //Assert
+            action.Should().ThrowExactly<ArgumentNullException>();
+        }
+
+        [Test]
+        [TestCase(0, 0, -1)]
+        [TestCase(0, 0, 1)]
+        public void GetColumn_RowIndexInvalid_ThrowArgumentOutOfRangeException(int rows, int columns, int row)
+        {
+            //Arrange
+            var source = new int[rows, columns];
+
+            //Assert
+            Action action = () => source.GetColumn(row);
+
+            //Assert
+            action.Should().ThrowExactly<ArgumentOutOfRangeException>();
+        }
+
+        [Test]
+        public void GetColumn_WhenCalled_ReturnRow()
+        {
+            //Arrange
+            var source = new int[2, 2]
+            {
+                { 1, 2 },
+                { 3, 4 }
+            };
+            var expected = new[] { 2, 4 };
+
+            //Assert
+            var result = source.GetColumn(1);
+
+            //Assert
+            result.Should().Equal(expected);
+        }
+    }
+}
---
+++ CoreSharp/Extensions/ArrayExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CoreSharp.Extensions
+{
+    /// <summary>
+    /// Array extensions. 
+    /// </summary>
+    public static partial class ArrayExtensions
+    {
+        /// <summary>
+        /// Get row from 2D array. 
+        /// </summary> 
+        public static IEnumerable<T> GetRow<T>(this T[,] source, int row)
+        {
+            source = source ?? throw new ArgumentNullException(nameof(source));
+            int rowSize = source.GetLength(0);
+            if (row < 0 || row >= rowSize)
+                throw new ArgumentOutOfRangeException(nameof(row), $"{nameof(row)} has to be between 0 and {rowSize - 1}.");
+
+            return Enumerable
+                .Range(0, source.GetLength(1))
+                .Select(x => source[row, x]);
+        }
+
+        /// <summary>
+        /// Get column from 2D array. 
+        /// </summary> 
+        public static IEnumerable<T> GetColumn<T>(this T[,] source, int column)
+        {
+            source = source ?? throw new ArgumentNullException(nameof(source));
+            int columnSize = source.GetLength(1);
+            if (column < 0 || column >= columnSize)
+                throw new ArgumentOutOfRangeException(nameof(column), $"{nameof(column)} has to be between 0 and {columnSize - 1}.");
+
+            return Enumerable
+                        .Range(0, source.GetLength(0))
+                        .Select(x => source[x, column]);
+        }
+    }
+}
