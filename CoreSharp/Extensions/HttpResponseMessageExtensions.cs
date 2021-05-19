3171980b087b068e01703459f270142b50424401
Efthymios Koktsidis <efthymios.koktsidis@onelity.com>
2021-05-19 12:36:25 +0300

    Added HttpResponseMessage.EnsureSuccessStatusCodeAsync extension

---
+++ CoreSharp/Extensions/HttpResponseMessageExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using CoreSharp.Models;
+
+namespace CoreSharp.Extensions
+{
+    /// <summary>
+    /// HttpResponseMessage extensions. 
+    /// </summary>
+    public static partial class HttpResponseMessageExtensions
+    {
+        /// <summary>
+        /// Ensure http response was successful. 
+        /// Throws HttpResponseException if not, including HttpStatusCode and Content. 
+        /// </summary> 
+        public async static Task EnsureSuccessStatusCodeAsync(this HttpResponseMessage response)
+        {
+            if (response.IsSuccessStatusCode)
+                return;
+
+            var content = await response?.Content?.ReadAsStringAsync();
+            response?.Content?.Dispose();
+
+            throw new HttpResponseException(response.StatusCode, content);
+        }
+    }
+}
---
+++ CoreSharp/Models/HttpResponseException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net;
+
+namespace CoreSharp.Models
+{
+    /// <summary>
+    /// Simple HttpResponseMessage exception. 
+    /// Stores a HttpStatusCode and Content (string). 
+    /// </summary>
+    public class HttpResponseException : Exception
+    {
+        //Constructors
+        public HttpResponseException(HttpStatusCode statusCode, string content) : base(content)
+        {
+            StatusCode = statusCode;
+        }
+
+        //Properties
+        public HttpStatusCode StatusCode { get; }
+    }
+}
