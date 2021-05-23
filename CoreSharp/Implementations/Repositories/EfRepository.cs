7875649d5fe08377d3957ef82dd2d9e8a3d3fef6
Efthymios Koktsidis <efthymios.ks@gmail.com>
2021-05-23 20:53:28 +0300

    Changed EfRepository.IQueryable from private to protected

--- CoreSharp/Implementations/Repositories/EfRepository.cs
+++ CoreSharp/Implementations/Repositories/EfRepository.cs
@@ -11,19 +11,18 @@ namespace CoreSharp.Implementations.Repositories
 {
     public abstract class EfRepository<TEntity> : IRepository<TEntity> where TEntity : class, IEntity
     {
-        //Fields
-        private readonly DbSet<TEntity> table;
-
         //Constructors
         public EfRepository(DbContext context)
         {
             Context = context ?? throw new ArgumentNullException(nameof(context));
 
-            table = Context.Set<TEntity>();
+            Table = Context.Set<TEntity>();
         }
 
         //Properties 
         protected DbContext Context { get; }
+
+        protected DbSet<TEntity> Table { get; }
 
         //Methods 
         public async virtual Task<TEntity> GetAsync(object key, Func<IQueryable<TEntity>, IQueryable<TEntity>> navigation = null)
@@ -36,7 +35,7 @@ namespace CoreSharp.Implementations.Repositories
 
         public async virtual Task<IEnumerable<TEntity>> GetAsync(Expression<Func<TEntity, bool>> filter = null, Func<IQueryable<TEntity>, IQueryable<TEntity>> navigation = null)
         {
-            var query = table.AsQueryable();
+            var query = Table.AsQueryable();
             if (navigation != null)
                 query = navigation(query);
             if (filter != null)
@@ -50,7 +49,7 @@ namespace CoreSharp.Implementations.Repositories
             entity = entity ?? throw new ArgumentNullException(nameof(entity));
 
             entity.DateCreated = DateTime.UtcNow;
-            await table.AddAsync(entity);
+            await Table.AddAsync(entity);
         }
 
         public virtual Task UpdateAsync(TEntity entity)
@@ -58,7 +57,7 @@ namespace CoreSharp.Implementations.Repositories
             entity = entity ?? throw new ArgumentNullException(nameof(entity));
 
             entity.DateModified = DateTime.UtcNow;
-            table.Attach(entity);
+            Table.Attach(entity);
             Context.Entry(entity).State = EntityState.Modified;
 
             return Task.CompletedTask;
@@ -68,7 +67,7 @@ namespace CoreSharp.Implementations.Repositories
         {
             entity = entity ?? throw new ArgumentNullException(nameof(entity));
 
-            table.Remove(entity);
+            Table.Remove(entity);
 
             return Task.CompletedTask;
         }
