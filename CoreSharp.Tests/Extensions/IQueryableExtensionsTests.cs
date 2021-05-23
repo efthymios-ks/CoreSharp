a6d1b7b4d7a2c203877a445a8a95e4fcd68c4c74
Efthymios Koktsidis <efthymios.ks@gmail.com>
2021-05-23 21:15:38 +0300

    Added IQueryable.QueryPage extension

---
+++ CoreSharp.Tests/Extensions/IQueryableExtensionsTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using CoreSharp.Tests.Dummies;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace CoreSharp.Extensions.Tests
+{
+    [TestFixture]
+    public class IQueryableExtensionsTests
+    {
+        //Fields
+        private readonly IQueryable<DummyClass> sourceNull = null;
+
+        //Methods
+        [Test]
+        public void QueryPage_SourceIsNull_ThrowArgumentNullException()
+        {
+            //Arrange
+            int pageIndex = 0;
+            int pageSize = 0;
+
+            //Act 
+            Action action = () => sourceNull.QueryPage(pageIndex, pageSize);
+
+            //Assert 
+            action.Should().ThrowExactly<ArgumentNullException>();
+        }
+
+        [Test]
+        [TestCase(5, -1, 1)]
+        [TestCase(5, 1, -1)]
+        public void QueryPage_PageArgsIsOutOfRange_ThrowArgumentNullException(int itemCount, int pageIndex, int pageSize)
+        {
+            //Arrange
+            var source = new int[itemCount].AsQueryable();
+
+            //Act 
+            Action action = () => source.QueryPage(pageIndex, pageSize);
+
+            //Assert 
+            action.Should().ThrowExactly<ArgumentOutOfRangeException>();
+        }
+
+        [Test]
+        public void QueryPage_WhenCalled_ReturnItemPage()
+        {
+            //Arrange
+            var source = new[] { 1, 2, 3, 4, 5 }.AsQueryable();
+            var pageIndex = 1;
+            var pageSize = 2;
+            var expected = new[] { 3, 4 }.AsQueryable();
+
+            //Act 
+            var result = source.QueryPage(pageIndex, pageSize);
+
+            //Assert 
+            result.Should().Equal(expected);
+        }
+    }
+}
---
+++ CoreSharp/Extensions/IQueryableExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CoreSharp.Extensions
+{
+    /// <summary>
+    /// IQueryable extensions. 
+    /// </summary>
+    public static partial class IQueryableExtensions
+    {
+        /// <summary>
+        /// Paginate collection on given size and return page of given index. 
+        /// </summary> 
+        public static IEnumerable<T> QueryPage<T>(this IQueryable<T> source, int pageIndex, int pageSize)
+        {
+            source = source ?? throw new ArgumentNullException(nameof(source));
+            if (pageIndex < 0)
+                throw new ArgumentOutOfRangeException($"{pageIndex} has to be positive and non-zero.");
+            if (pageSize <= 0)
+                throw new ArgumentOutOfRangeException($"{pageSize} has to be positive and non-zero.");
+
+            return source.Skip(pageIndex * pageSize).Take(pageSize);
+        }
+    }
+}
