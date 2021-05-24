a1788ecee28d570b50fef8170f70b4f27146ad3e
Efthymios Koktsidis <efthymios.koktsidis@onelity.com>
2021-05-24 09:17:47 +0300

    Added Guid.IsNullOrEmpty extension

---
+++ CoreSharp/Extensions/GuidExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CoreSharp.Extensions
+{
+    /// <summary>
+    /// Guid extensions. 
+    /// </summary>
+    public static partial class GuidExtensions
+    {
+        /// <summary>
+        /// Compare given value with Guid.Empty (all zeros). 
+        /// </summary> 
+        public static bool IsNullOrEmpty(this Guid guid)
+        {
+            return guid == Guid.Empty;
+        }
+
+        /// <summary>
+        /// Compare given value with Guid.Empty (all zeros). 
+        /// </summary> 
+        public static bool IsNullOrEmpty(this Guid? guid)
+        {
+            guid ??= Guid.Empty;
+            return guid.Value.IsNullOrEmpty();
+        }
+    }
+}
