8d8a92372d647ae746d3a1449bd86efb16a864ea
Efthymios Koktsidis <efthymios.koktsidis@onelity.com>
2021-08-02 17:06:23 +0300

    Extensions, ExpandoObject, Initial commit

---
+++ CoreSharp.Tests/Extensions/ExpandoObjectExtensionsTests.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+
+namespace RestClient.Extensions.Tests
+{
+    [TestFixture]
+    public class ExpandoObjectExtensionsTests
+    {
+        [Test]
+        public void ToDictionary_InputIsNull_ThrowNewArgumentNullException()
+        {
+            //Arrange 
+            ExpandoObject expando = null;
+
+            //Act 
+            Action action = () => expando.ToDictionary();
+
+            //Assert
+            action.Should().ThrowExactly<ArgumentNullException>();
+        }
+
+        [Test]
+        public void ToDictionary_WhenCalled_ReturnDictionary()
+        {
+            //Arrange 
+            var source = new Dictionary<string, object>();
+            source.Add("id", 1);
+            source.Add("color", "black");
+            var expando = new ExpandoObject() as IDictionary<string, object>;
+            foreach (var item in source)
+                expando.Add(item);
+
+            //Act 
+            var dictionary = ((ExpandoObject)expando).ToDictionary();
+
+            //Assert
+            dictionary.Should().Equal(source);
+        }
+    }
+}
