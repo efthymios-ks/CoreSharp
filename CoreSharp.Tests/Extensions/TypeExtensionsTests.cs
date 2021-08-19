a579f146d904a56c3383cb4e4f745eb244593529
Efthymios Koktsidis <efthymios.koktsidis@onelity.com>
2021-08-19 09:54:06 +0300

    Extensions, Added TypeExtensions

---
+++ CoreSharp.Tests/Extensions/TypeExtensionsTests.cs
@@ -0,0 +1,66 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+
+namespace CoreSharp.Extensions.Tests
+{
+    [TestFixture]
+    public class TypeExtensionsTests
+    {
+        [Test]
+        public void IsNumeric_TypeIsNull_ThrowArgumentNullException()
+        {
+            //Arrange
+            Type typeNull = null;
+
+            //Act
+            Action action = () => typeNull.IsNumeric();
+
+            //Assert
+            action.Should().ThrowExactly<ArgumentNullException>();
+        }
+
+        [Test]
+        [TestCase(typeof(double), true)]
+        [TestCase(typeof(double?), true)]
+        [TestCase(typeof(int), true)]
+        [TestCase(typeof(int?), true)]
+        [TestCase(typeof(string), false)]
+        public void IsNumeric_TypeIsNumeric_ReturnTrue(Type type, bool expected)
+        {
+            //Act
+            bool result = type.IsNumeric();
+
+            //Assert
+            result.Should().Be(expected);
+        }
+
+        [Test]
+        public void IsDate_TypeIsNull_ThrowArgumentNullException()
+        {
+            //Arrange
+            Type typeNull = null;
+
+            //Act
+            Action action = () => typeNull.IsDate();
+
+            //Assert
+            action.Should().ThrowExactly<ArgumentNullException>();
+        }
+
+        [Test]
+        [TestCase(typeof(DateTime), true)]
+        [TestCase(typeof(DateTime?), true)]
+        [TestCase(typeof(DateTimeOffset), true)]
+        [TestCase(typeof(DateTimeOffset?), true)]
+        [TestCase(typeof(string), false)]
+        public void IsDate_TypeIsDate_ReturnTrue(Type type, bool expected)
+        {
+            //Act
+            bool result = type.IsDate();
+
+            //Assert
+            result.Should().Be(expected);
+        }
+    }
+}
---
+++ CoreSharp/Extensions/TypeExtensions.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace CoreSharp.Extensions
+{
+    /// <summary>
+    /// Type extensions. 
+    /// </summary>
+    public static class TypeExtensions
+    {
+        /// <summary>
+        /// Check if given type is numeric. 
+        /// </summary> 
+        public static bool IsNumeric(this Type type)
+        {
+            _ = type ?? throw new ArgumentNullException(nameof(type));
+
+            switch (Type.GetTypeCode(type))
+            {
+                case TypeCode.Byte:
+                case TypeCode.SByte:
+                case TypeCode.Int16:
+                case TypeCode.Int32:
+                case TypeCode.Int64:
+                case TypeCode.UInt16:
+                case TypeCode.UInt32:
+                case TypeCode.UInt64:
+                case TypeCode.Single:
+                case TypeCode.Double:
+                case TypeCode.Decimal:
+                    return true;
+                case TypeCode.Object:
+                    {
+                        var baseType = Nullable.GetUnderlyingType(type);
+                        return baseType?.IsNumeric() ?? false;
+                    }
+                default:
+                    return false;
+            }
+        }
+
+        /// <summary>
+        /// Check if given type is DateTime(Offset). 
+        /// </summary> 
+        public static bool IsDate(this Type type)
+        {
+            _ = type ?? throw new ArgumentNullException(nameof(type));
+
+            switch (Type.GetTypeCode(type))
+            {
+                case TypeCode.DateTime:
+                    return true;
+                case TypeCode.Object:
+                    {
+                        if (type == typeof(DateTimeOffset))
+                            return true;
+                        else
+                        {
+                            var baseType = Nullable.GetUnderlyingType(type);
+                            return baseType?.IsDate() ?? false;
+                        }
+                    }
+                default:
+                    return false;
+            }
+        }
+    }
+}
