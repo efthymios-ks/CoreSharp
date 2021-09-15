2a77a44495312a03d1a62bfc3a0c6ba335c16165
Efthymios Koktsidis <efthymios.ks@gmail.com>
2021-09-15 17:53:37 +0300

    Utilities, String, Added FirstNotEmpty() method

---
+++ CoreSharp.Tests/Utilities/StringTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System.Linq;
+
+namespace CoreSharp.Utilities.Tests
+{
+    [TestFixture]
+    public class StringTests
+    {
+        [Test]
+        public void FirstNotEmpty_WhenCalled_ReturbFirstNotEmpty()
+        {
+            //Arrange 
+            var emptyValues = new[] { null, string.Empty, "", " " };
+            var expected = "1";
+            var values = emptyValues.Append(expected).Concat(emptyValues);
+
+            //Act 
+            var result = String.FirstNotEmpty(values);
+
+            //Assert 
+            result.Should().Be(expected);
+        }
+    }
+}
---
+++ CoreSharp/Utilities/String.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CoreSharp.Utilities
+{
+    /// <summary>
+    /// String utilities. 
+    /// </summary>
+    public static class String
+    {
+        /// <inheritdoc cref="FirstNotEmpty(string[])"/>
+        public static string FirstNotEmpty(IEnumerable<string> values)
+            => FirstNotEmpty(values?.ToArray());
+
+        /// <summary>
+        /// Return first value not null or whitespace. 
+        /// </summary> 
+        public static string FirstNotEmpty(params string[] values)
+            => values?.FirstOrDefault(v => !string.IsNullOrWhiteSpace(v));
+    }
+}
