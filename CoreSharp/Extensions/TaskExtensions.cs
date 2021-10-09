aa4a5a02ed3c907d1ab9be4f6016282626fa5177
Efthymios <efthymios.ks@gmail.com>
2021-10-09 10:14:17 +0300

    Extensions, Tasks, Added AwaitResult() method

---
+++ CoreSharp/Extensions/TaskExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace CoreSharp.Extensions
+{
+    /// <summary>
+    /// <see cref="Task"/> extensions.
+    /// </summary>
+    public static class TaskExtensions
+    {
+        /// <summary>
+        /// Chain calls <see cref="Task{TResult}.GetAwaiter"/> and <see cref="TaskAwaiter.GetResult"/>
+        /// </summary>
+        public static TResult AwaitResult<TResult>(this Task<TResult> task)
+        {
+            _ = task ?? throw new ArgumentNullException(nameof(task));
+            return task.GetAwaiter().GetResult();
+        }
+    }
+}
