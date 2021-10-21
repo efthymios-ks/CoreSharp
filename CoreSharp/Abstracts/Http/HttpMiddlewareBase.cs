63a8cc3891a4be59e6814e0a57c3e82681978279
Efthymios <efthymios.ks@gmail.com>
2021-10-21 18:58:39 +0300

    Abstracts, Added MiddlewareBase

---
+++ CoreSharp/Abstracts/Http/HttpMiddlewareBase.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace CoreSharp.Abstracts.Http
+{
+    public abstract class HttpMiddlewareBase
+    {
+        //Constructors
+        protected HttpMiddlewareBase(RequestDelegate next)
+        {
+            Next = next ?? throw new ArgumentNullException(nameof(next));
+        }
+
+        //Properties
+        public RequestDelegate Next { get; }
+
+        //Methods
+        public abstract Task InvokeAsync(HttpContext context);
+    }
+}
