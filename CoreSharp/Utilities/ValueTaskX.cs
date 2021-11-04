68ac44c4bc047b5c6fd69d8237d9fa3beb04a8a9
Efthymios Koktsidis <efthymios.koktsidis@onelity.com>
2021-11-04 15:50:59 +0200

    Utilities, Added ValueTaskX

---
+++ CoreSharp/Utilities/ValueTaskX.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreSharp.Utilities
+{
+    /// <summary>
+    /// <see cref="ValueTask"/> utilities.
+    /// </summary>
+    public static class ValueTaskX
+    {
+        /// <inheritdoc cref="WithAll(ValueTask[])"/>
+        public static async Task WithAll(IEnumerable<ValueTask> valueTasks)
+            => await WithAll(valueTasks?.ToArray());
+
+        /// <inheritdoc cref="Task.WhenAll(Task[])"/>
+        public static async Task WithAll(params ValueTask[] valueTasks)
+        {
+            _ = valueTasks ?? throw new ArgumentNullException(nameof(valueTasks));
+
+            var tasks = valueTasks.Select(vt => vt.AsTask());
+            await Task.WhenAll(tasks);
+        }
+    }
+}
