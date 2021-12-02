508f7e2f75b3002a016865e198ce9abe128ddbae
Efthymios Koktsidis <efthymios.koktsidis@onelity.com>
2021-12-02 11:36:37 +0200

    Utilities, HashSet, Added `New()` methods

---
+++ CoreSharp/Models/KeyEqualityComparer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CoreSharp.Models
+{
+    public class KeyEqualityComparer<TItem, TKey> : IEqualityComparer<TItem>
+    {
+        //Constructors
+        public KeyEqualityComparer(Func<TItem, TKey> keySelector)
+            => KeySelector = keySelector ?? throw new ArgumentNullException(nameof(keySelector));
+
+        //Properties
+        protected Func<TItem, TKey> KeySelector { get; }
+
+        //Methods
+        public virtual bool Equals(TItem left, TItem right)
+            => EqualityComparer<TKey>.Default.Equals(KeySelector(left), KeySelector(right));
+
+        public int GetHashCode(TItem item)
+            => KeySelector(item).GetHashCode();
+    }
+}
---
+++ CoreSharp/Utilities/HashSetX.cs
@@ -0,0 +1,40 @@
+﻿using CoreSharp.Models;
+using System;
+using System.Collections.Generic;
+
+namespace CoreSharp.Utilities
+{
+    /// <summary>
+    /// <see cref="HashSet{T}"/> utilities.
+    /// </summary>
+    public static class HashSetX
+    {
+        /// <inheritdoc cref="HashSet{T}.HashSet(IEqualityComparer{T}?)" />
+        public static HashSet<TItem> New<TItem, TKey>(Func<TItem, TKey> keySelector)
+        {
+            _ = keySelector ?? throw new ArgumentNullException(nameof(keySelector));
+
+            var comparer = new KeyEqualityComparer<TItem, TKey>(keySelector);
+            return new HashSet<TItem>(comparer);
+        }
+
+        /// <inheritdoc cref="HashSet{T}.HashSet(int, IEqualityComparer{T}?)" />
+        public static HashSet<TItem> New<TItem, TKey>(int capacity, Func<TItem, TKey> keySelector)
+        {
+            _ = keySelector ?? throw new ArgumentNullException(nameof(keySelector));
+
+            var comparer = new KeyEqualityComparer<TItem, TKey>(keySelector);
+            return new HashSet<TItem>(capacity, comparer);
+        }
+
+        /// <inheritdoc cref="HashSet{T}.HashSet(IEnumerable{T}, IEqualityComparer{T}?)" />
+        public static HashSet<TItem> New<TItem, TKey>(IEnumerable<TItem> source, Func<TItem, TKey> keySelector)
+        {
+            _ = keySelector ?? throw new ArgumentNullException(nameof(keySelector));
+            _ = source ?? throw new ArgumentNullException(nameof(source));
+
+            var comparer = new KeyEqualityComparer<TItem, TKey>(keySelector);
+            return new HashSet<TItem>(source, comparer);
+        }
+    }
+}
