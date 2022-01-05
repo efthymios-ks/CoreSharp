0c9572a27011a54fe11fb01e7a2a77ff47234e2b
Efthymios Koktsidis <efthymios.koktsidis@onelity.com>
2022-01-05 11:27:28 +0200

    Utilities, Added EnvironmentX.

---
+++ CoreSharp/Utilities/EnvironmentX.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CoreSharp.Utilities
+{
+    /// <summary>
+    /// <see cref="Environment"/> utilities.
+    /// </summary>
+    public static class EnvironmentX
+    {
+        //Methods
+        /// <summary>
+        /// Get ASPNETCORE_ENVIRONMENT variable.
+        /// </summary> 
+        private static string GetAspNetCoreEnvironment()
+            => Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+        /// <summary>
+        /// Checks if ASPNETCORE_ENVIRONMENT == DEVELOPMENT.
+        /// </summary> 
+        public static bool IsDevelopment()
+            => string.Equals(GetAspNetCoreEnvironment(), "Development", StringComparison.InvariantCultureIgnoreCase);
+    }
+}
