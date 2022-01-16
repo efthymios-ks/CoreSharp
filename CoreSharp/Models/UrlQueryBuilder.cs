a8bdd3be54e411dec3df7b945b81f0e1b7ffd7ea
Efthymios <efthymios.ks@gmail.com>
2022-01-16 12:23:24 +0200

    Models, UrlQueryBuilder, Code clean-up.

--- CoreSharp/Models/UrlQueryBuilder.cs
+++ CoreSharp/Models/UrlQueryBuilder.cs
@@ -1,9 +1,10 @@
-﻿using CoreSharp.Extensions;
-using Microsoft.AspNetCore.Http.Extensions;
+﻿using Microsoft.AspNetCore.Http.Extensions;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
+using System.Reflection;
 
 namespace CoreSharp.Models
 {
@@ -16,41 +17,6 @@ namespace CoreSharp.Models
                 base.Add(key, Convert.ToString(value, CultureInfo.InvariantCulture));
         }
 
-        //Basic 
-        public void Add<TValue>(string key, TValue value)
-        {
-            if (value is null)
-                return;
-
-            var type = typeof(TValue);
-            switch (value)
-            {
-                case DateTime dateTimeValue:
-                    Add(key, dateTimeValue);
-                    break;
-                case DateTimeOffset dateTimeOffsetValue:
-                    Add(key, dateTimeOffsetValue);
-                    break;
-                case TimeSpan timeSpanValue:
-                    Add(key, timeSpanValue);
-                    break;
-                case string stringValue when !string.IsNullOrWhiteSpace(stringValue):
-                    InternalAdd(key, stringValue.Trim());
-                    break;
-                case IEnumerable enumerable:
-                    Add(key, enumerable);
-                    break;
-                default:
-                    if (value is IEnumerable<object> enumerableObject)
-                        Add(key, enumerableObject);
-                    else if (type.IsClass && value is not object)
-                        Add(((object)value).GetPropertiesDictionary());
-                    else
-                        InternalAdd(key, value);
-                    break;
-            }
-        }
-
         /// <summary>
         /// Add <see cref="IDictionary{TKey, TValue}"/>.
         /// </summary>
@@ -59,54 +25,50 @@ namespace CoreSharp.Models
             _ = dictionary ?? throw new ArgumentNullException(nameof(dictionary));
 
             foreach (var pair in dictionary)
-                Add(pair.Key, pair.Value);
+                InternalAdd(pair.Key, pair.Value);
         }
 
         /// <summary>
         /// Split item into properties and add one-by-one.
         /// </summary>
-        public void Parse<TItem>(TItem item) where TItem : class
+        public void Parse<TItem>(TItem item)
+            where TItem : class
         {
             _ = item ?? throw new ArgumentNullException(nameof(item));
 
-            var properties = item.GetPropertiesDictionary();
+            var properties = item.GetType()
+                                 .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                                 .ToDictionary(p => p.Name, p => p.GetValue(item));
             Add(properties);
         }
 
-        //Type-specific 
         /// <summary>
         /// Add <see cref="DateTime"/> and format with "s" specifier.
         /// </summary>
-        private void Add(string key, DateTime value)
+        public void Add(string key, DateTime value)
             => InternalAdd(key, value.ToString("s"));
 
         /// <summary>
         /// Add <see cref="DateTimeOffset"/> and format with "s" specifier.
         /// </summary>
-        private void Add(string key, DateTimeOffset value)
+        public void Add(string key, DateTimeOffset value)
             => InternalAdd(key, value.ToString("s"));
 
         /// <summary>
         /// Add <see cref="TimeSpan"/> and format with "s" specifier.
         /// </summary>
-        private void Add(string key, TimeSpan value)
+        public void Add(string key, TimeSpan value)
             => InternalAdd(key, value.ToString("c"));
-
-        /// <summary>
-        /// Add <see cref="IEnumerable{T}"/>.
-        /// </summary>
-        private void Add<TValue>(string key, IEnumerable<TValue> items)
-            => Add(key, (IEnumerable)items);
 
         /// <summary>
         /// Add <see cref="IEnumerable"/>.
         /// </summary>
-        private void Add(string key, IEnumerable items)
+        public void Add(string key, IEnumerable items)
         {
             _ = items ?? throw new ArgumentNullException(nameof(items));
 
             foreach (var item in items)
-                Add(key, item);
+                InternalAdd(key, item);
         }
     }
 }
