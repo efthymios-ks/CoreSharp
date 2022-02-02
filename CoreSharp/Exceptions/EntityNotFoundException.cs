e148ff35d77d28d00154aa44daf7876da590f2be
Efthymios Koktsidis <efthymios.koktsidis@onelity.com>
2022-02-02 10:06:07 +0200

    Exceptions, Added `EntityNotFoundException`.

---
+++ CoreSharp/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,37 @@
+﻿using CoreSharp.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace CoreSharp.Exceptions
+{
+    public class EntityNotFoundException : KeyNotFoundException
+    {
+        //Constructors
+        public EntityNotFoundException(Type entityType, string keyName, object keyValue)
+            : this(entityType.Name, keyName, keyValue)
+        {
+        }
+
+        public EntityNotFoundException(string entityName, string keyName, object keyValue)
+            : this($"{entityName} with {keyName}=`{keyValue}` not found.")
+        {
+        }
+
+        public EntityNotFoundException(string message)
+            : base(message)
+        {
+        }
+
+        //Methods 
+        public static void Throw<TEntity, TKey>(Expression<Func<TEntity, TKey>> keySelector, TKey keyValue)
+        {
+            _ = keySelector ?? throw new ArgumentNullException(nameof(keySelector));
+
+            var entityType = typeof(TEntity);
+            var memberName = keySelector.GetMemberName();
+
+            throw new EntityNotFoundException(entityType, memberName, keyValue);
+        }
+    }
+}
