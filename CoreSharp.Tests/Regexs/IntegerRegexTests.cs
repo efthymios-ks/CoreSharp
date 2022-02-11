2dbdd48c541ecb240cb1dbaa6ef32cadec4b808b
Efthymios Koktsidis <efthymios.koktsidis@onelity.com>
2022-02-11 15:46:47 +0200

    Regexs, Added IntegerRegex.

---
+++ CoreSharp.Tests/Regexs/IntegerRegexTests.cs
@@ -0,0 +1,76 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System.Globalization;
+
+namespace CoreSharp.Regexs.Tests
+{
+    [TestFixture]
+    public class IntegerRegexTests
+    {
+        [Test]
+        [TestCase("-1", "el-GR", true)]
+        [TestCase("1", "el-GR", true)]
+        [TestCase("+1", "el-GR", true)]
+        [TestCase("-1.0", "el-GR", false)]
+        [TestCase("1.0", "el-GR", false)]
+        [TestCase("+1.0", "el-GR", false)]
+        [TestCase("-1,0", "el-GR", false)]
+        [TestCase("1,0", "el-GR", false)]
+        [TestCase("+1,0", "el-GR", false)]
+        [TestCase("+1.000.000", "el-GR", true)]
+        [TestCase("1.000.000", "el-GR", true)]
+        [TestCase("+1.000.000", "el-GR", true)]
+        [TestCase("+1,000,000", "el-GR", false)]
+        [TestCase("1,000,000", "el-GR", false)]
+        [TestCase("+1,000,000", "el-GR", false)]
+        [TestCase("+1.000.000,0", "el-GR", false)]
+        [TestCase("1.000.000,0", "el-GR", false)]
+        [TestCase("+1.000.000,0", "el-GR", false)]
+        [TestCase("+1,000,000.0", "el-GR", false)]
+        [TestCase("1,000,000.0", "el-GR", false)]
+        [TestCase("+1,000,000.0", "el-GR", false)]
+        public void IntegerRegex_WhenInteger_ReturnTrue(string input, string cultureName, bool isMatch)
+        {
+            //Arrange
+            var cultureInfo = CultureInfo.GetCultureInfo(cultureName);
+            var integerRegex = new IntegerRegex(input, cultureInfo);
+
+            //Act 
+            var result = integerRegex.IsMatch;
+
+            result.Should().Be(isMatch);
+        }
+
+        [Test]
+        [TestCase("-1", "el-GR", '-')]
+        [TestCase("1", "el-GR", null)]
+        [TestCase("+1", "el-GR", '+')]
+        public void IntegerRegex_WhenCalled_MatchGroupSign(string input, string cultureName, char? groupSign)
+        {
+            //Arrange
+            var cultureInfo = CultureInfo.GetCultureInfo(cultureName);
+            var integerRegex = new IntegerRegex(input, cultureInfo);
+
+            //Act 
+            var result = integerRegex.Sign;
+
+            result.Should().Be(groupSign);
+        }
+
+        [Test]
+        [TestCase("-1", "el-GR", "1")]
+        [TestCase("1", "el-GR", "1")]
+        [TestCase("+1", "el-GR", "1")]
+        public void IntegerRegex_WhenCalled_MatchGroupValue(string input, string cultureName, string groupValue)
+        {
+            //Arrange
+            var cultureInfo = CultureInfo.GetCultureInfo(cultureName);
+            var integerRegex = new IntegerRegex(input, cultureInfo);
+
+            //Act 
+            var result = integerRegex.Value;
+
+            result.Should().Be(groupValue);
+        }
+    }
+}
---
+++ CoreSharp/Utilities/RegexX.cs
@@ -0,0 +1,43 @@
+﻿using CoreSharp.Regexs;
+using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace CoreSharp.Utilities
+{
+    /// <summary>
+    /// <see cref="Regex"/> utilities.
+    /// </summary>
+    public static class RegexX
+    {
+        //Methods
+        /// <summary>
+        /// Reduces many consequtive occurences of a char to single one.
+        /// <code>
+        /// var input = "11.2.3.4";
+        /// var result = RegexX.ReduceMany(input, '1'); // 1.2.3.4
+        /// </code>
+        /// </summary>
+        public static string ReduceMany(string input, char occurence)
+        {
+            _ = input ?? throw new ArgumentNullException(nameof(input));
+
+            var escapedOccurence = Regex.Escape($"{occurence}");
+            var pattern = $"{escapedOccurence}{{2,}}";
+            return Regex.Replace(input, pattern, $"{occurence}");
+        }
+
+        /// <inheritdoc cref="IsInteger(string, CultureInfo)"/>
+        public static bool IsInteger(string input)
+            => IsInteger(input, CultureInfo.CurrentCulture);
+
+        /// <inheritdoc cref="IntegerRegex"/>
+        public static bool IsInteger(string input, CultureInfo cultureInfo)
+        {
+            _ = input ?? throw new ArgumentNullException(nameof(input));
+            _ = cultureInfo ?? throw new ArgumentNullException(nameof(cultureInfo));
+
+            return new IntegerRegex(input, cultureInfo).IsMatch;
+        }
+    }
+}
