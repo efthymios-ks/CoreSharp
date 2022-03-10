429eb339311119c9fdf069469c561e3aa44017ae
Efthymios Koktsidis <efthymios.koktsidis@onelity.com>
2022-03-10 10:01:36 +0200

    Models, Pages, Added `PageOptions`.

---
+++ CoreSharp/Models/Pages/PageOptions.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+
+namespace CoreSharp.Models.Pages
+{
+    public class PageOptions
+    {
+        //Fields
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        private const int DefaultPageNumber = default;
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        private const int DefaultPageSize = 20;
+
+        //Properties 
+        [DefaultValue(DefaultPageNumber)]
+        public int PageNumber { get; set; } = DefaultPageNumber;
+
+        [DefaultValue(DefaultPageSize)]
+        public int PageSize { get; set; } = DefaultPageSize;
+    }
+}
