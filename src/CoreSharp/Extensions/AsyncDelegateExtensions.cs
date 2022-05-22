42dfedad23d21839cba575e4407a90c7ff3bd45b
Efthymios <efthymios.ks@gmail.com>
2022-05-22 11:05:27 +0300

    Delegates, Added `AsyncDelegate`.

---
+++ src/CoreSharp/Extensions/AsyncDelegateExtensions.cs
@@ -0,0 +1,25 @@
+﻿using CoreSharp.Delegates;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CoreSharp.Extensions
+{
+    /// <summary>
+    /// <see cref="AsyncDelegate"/> extensions.
+    /// </summary>
+    public static class AsyncDelegateExtensions
+    {
+        /// <inheritdoc cref="AsyncEventHandlerExtensions.InvokeAsync{TEventArgs}(AsyncEventHandler{TEventArgs}, object, TEventArgs, CancellationToken)"/>
+        public static async Task InvokeAsync(this AsyncDelegate handler, CancellationToken cancellationToken = default)
+        {
+            var delegates = handler?.GetInvocationList();
+            if (delegates?.Length > 0)
+            {
+                var tasks = delegates.Cast<AsyncDelegate>()
+                                     .Select(e => e.Invoke(cancellationToken));
+                await Task.WhenAll(tasks);
+            }
+        }
+    }
+}
