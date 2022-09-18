d1300081a8fa14aed1a0c432c5969af8cdd508c0
Efthymios <efthymios.ks@gmail.com>
2022-09-18 13:39:31 +0300

    Collections, ObservableHashSet, Added tests.

--- src/CoreSharp/Collections/ObservableHashSet`1.cs
+++ src/CoreSharp/Collections/ObservableHashSet`1.cs
@@ -1,93 +1,40 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Collections.Specialized;
 
 namespace CoreSharp.Collections;
 
-public class ObservableHashSet<TEntity> : ICollection<TEntity>, INotifyCollectionChanged
+public sealed class ObservableHashSet<TEntity> : HashSet<TEntity>, INotifyCollectionChanged
 {
-    // Fields
-    private readonly HashSet<TEntity> _source;
-
-    // Constructors
-    public ObservableHashSet()
-        : this(EqualityComparer<TEntity>.Default)
-    {
-    }
-
-    public ObservableHashSet(IEnumerable<TEntity> source)
-        : this(source, EqualityComparer<TEntity>.Default)
-    {
-    }
-
-    public ObservableHashSet(IEqualityComparer<TEntity> equalityComparer)
-        => _source = new(equalityComparer);
-
-    public ObservableHashSet(IEnumerable<TEntity> source, IEqualityComparer<TEntity> equalityComparer)
-        => _source = new(source, equalityComparer);
-
-    public ObservableHashSet(int capacity)
-        : this(capacity, EqualityComparer<TEntity>.Default)
-    {
-    }
-
-    public ObservableHashSet(int capacity, IEqualityComparer<TEntity> equalityComparer)
-        => _source = new(capacity, equalityComparer);
-
-    // Properties
-    public int Count
-        => _source.Count;
-
-    public bool IsReadOnly
-        => false;
-
     // Events 
     public event NotifyCollectionChangedEventHandler CollectionChanged;
 
     // Methods
-    protected void OnCollectionChanged(NotifyCollectionChangedEventArgs args)
+    public new void Add(TEntity item)
     {
-        _ = args ?? throw new ArgumentNullException(nameof(args));
-
-        CollectionChanged?.Invoke(this, args);
-    }
-
-    public bool Contains(TEntity item)
-        => _source.Contains(item);
-
-    public void CopyTo(TEntity[] array, int arrayIndex)
-        => _source.CopyTo(array, arrayIndex);
-
-    public void Add(TEntity item)
-    {
-        if (!_source.Add(item))
+        if (!base.Add(item))
             return;
 
         OnCollectionChanged(new(NotifyCollectionChangedAction.Add, new[] { item }));
     }
 
-    public void Clear()
+    public new bool Remove(TEntity item)
     {
-        if (_source.Count == 0)
-            return;
-
-        _source.Clear();
-        OnCollectionChanged(new(NotifyCollectionChangedAction.Reset));
-    }
-
-    public bool Remove(TEntity item)
-    {
-        if (!_source.Remove(item))
+        if (!base.Remove(item))
             return false;
 
         OnCollectionChanged(new(NotifyCollectionChangedAction.Remove, new[] { item }));
         return true;
     }
 
-    public IEnumerator<TEntity> GetEnumerator()
-        => _source.GetEnumerator();
+    public new void Clear()
+    {
+        if (Count == 0)
+            return;
 
-    IEnumerator IEnumerable.GetEnumerator()
-        => GetEnumerator();
+        base.Clear();
+        OnCollectionChanged(new(NotifyCollectionChangedAction.Reset));
+    }
+
+    private void OnCollectionChanged(NotifyCollectionChangedEventArgs args)
+        => CollectionChanged?.Invoke(this, args);
 }
---
+++ tests/Tests/Collections/ObservableHashSetTests.cs
@@ -0,0 +1,185 @@
+﻿using System.Collections.Specialized;
+using System.Diagnostics.CodeAnalysis;
+
+namespace CoreSharp.Collections.Tests;
+
+[TestFixture]
+public class ObservableHashSetTests
+{
+    [Test]
+    public void Add_ContainsItem_DoNothing()
+    {
+        // Arrange 
+        NotifyCollectionChangedEventArgs capturedArgs = null;
+        var originalItems = new[] { 1, 2 };
+        var hashSet = new ObservableHashSet<int>();
+        foreach (var item in originalItems)
+            hashSet.Add(item);
+        hashSet.CollectionChanged += (_, args)
+            => capturedArgs = args;
+
+        // Act
+        hashSet.Add(1);
+
+        // Assert 
+        capturedArgs.Should().BeNull();
+        hashSet.Should().Equal(originalItems);
+    }
+
+    [Test]
+    [SuppressMessage("Style", "IDE0028:Simplify collection initialization", Justification = "<Pending>")]
+    public void Add_DoesNotContainItem_AddItem()
+    {
+        // Arrange  
+        var hashSet = new ObservableHashSet<int> { 1, 2 };
+
+        // Act
+        hashSet.Add(3);
+
+        // Assert 
+        hashSet.Should().Equal(new[] { 1, 2, 3 });
+    }
+
+    [Test]
+    public void Add_DoesNotContainItem_NotifyItemAdded()
+    {
+        // Arrange 
+        NotifyCollectionChangedEventArgs capturedArgs = null;
+        var hashSet = new ObservableHashSet<int> { 1, 2 };
+        hashSet.CollectionChanged += (_, args)
+            => capturedArgs = args;
+
+        // Act
+        hashSet.Add(3);
+
+        // Assert 
+        capturedArgs.Should().NotBeNull();
+        capturedArgs.Action.Should().Be(NotifyCollectionChangedAction.Add);
+        capturedArgs.NewItems.Count.Should().Be(1);
+        capturedArgs.NewItems[0].Should().Be(3);
+    }
+
+    [Test]
+    public void Remove_DoesNotContainItem_DoNothing()
+    {
+        // Arrange 
+        NotifyCollectionChangedEventArgs capturedArgs = null;
+        var hashSet = new ObservableHashSet<int> { 1, 2 };
+        hashSet.CollectionChanged += (_, args)
+            => capturedArgs = args;
+
+        // Act
+        hashSet.Remove(3);
+
+        // Assert 
+        capturedArgs.Should().BeNull();
+        hashSet.Should().Equal(new[] { 1, 2 });
+    }
+
+    [Test]
+    public void Remove_DoesNotContainItem_ReturnFalse()
+    {
+        // Arrange  
+        var hashSet = new ObservableHashSet<int> { 1, 2 };
+
+        // Act
+        var removed = hashSet.Remove(3);
+
+        // Assert 
+        removed.Should().BeFalse();
+    }
+
+    [Test]
+    public void Remove_ContainsItem_RemoveItem()
+    {
+        // Arrange 
+        var hashSet = new ObservableHashSet<int> { 1, 2 };
+
+        // Act
+        hashSet.Remove(2);
+
+        // Assert 
+        hashSet.Should().Equal(new[] { 1 });
+    }
+
+    [Test]
+    public void Remove_ContainsItem_ReturnTrue()
+    {
+        // Arrange 
+        var hashSet = new ObservableHashSet<int> { 1, 2 };
+
+        // Act
+        var removed = hashSet.Remove(2);
+
+        // Assert 
+        removed.Should().BeTrue();
+    }
+
+    [Test]
+    public void Remove_ContainsItem_NotifyRemove()
+    {
+        // Arrange 
+        NotifyCollectionChangedEventArgs capturedArgs = null;
+        var hashSet = new ObservableHashSet<int> { 1, 2 };
+        hashSet.CollectionChanged += (_, args)
+            => capturedArgs = args;
+
+        // Act
+        hashSet.Remove(2);
+
+        // Assert 
+        capturedArgs.Should().NotBeNull();
+        capturedArgs.Action.Should().Be(NotifyCollectionChangedAction.Remove);
+        capturedArgs.OldItems.Count.Should().Be(1);
+        capturedArgs.OldItems[0].Should().Be(2);
+    }
+
+    [Test]
+    public void Clear_DoesNotContainItems_DoNothing()
+    {
+        // Arrange 
+        NotifyCollectionChangedEventArgs capturedArgs = null;
+        var hashSet = new ObservableHashSet<int>();
+        hashSet.CollectionChanged += (_, args)
+            => capturedArgs = args;
+
+        // Act
+        hashSet.Clear();
+
+        // Assert 
+        capturedArgs.Should().BeNull();
+        hashSet.Should().BeEmpty();
+    }
+
+    [Test]
+    public void Clear_ContainsItems_ClearItems()
+    {
+        // Arrange 
+        var hashSet = new ObservableHashSet<int> { 1 };
+
+        // Act
+        hashSet.Clear();
+
+        // Assert 
+        hashSet.Should().BeEmpty();
+    }
+
+    [Test]
+    public void Clear_ContainsItems_NotifyReset()
+    {
+        // Arrange 
+        NotifyCollectionChangedEventArgs capturedArgs = null;
+        var hashSet = new ObservableHashSet<int> { 1 };
+        hashSet.CollectionChanged += (_, args)
+            => capturedArgs = args;
+
+        // Act
+        hashSet.Clear();
+
+        // Assert 
+        capturedArgs.Should().NotBeNull();
+        capturedArgs.Action.Should().Be(NotifyCollectionChangedAction.Reset);
+        capturedArgs.OldItems.Should().BeNull();
+        capturedArgs.NewItems.Should().BeNull();
+    }
+}
