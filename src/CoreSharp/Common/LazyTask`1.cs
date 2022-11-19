2667085eef7c9eb995593960a3e395fa39b7f15e
Efthymios <efthymios.ks@gmail.com>
2022-11-19 10:51:31 +0200

    Common, Added `LazyTast`.

---
+++ src/CoreSharp/Common/LazyTask`1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CoreSharp.Common;
+
+public sealed class LazyTask<TValue>
+{
+    // Fields
+    private readonly Func<Task<TValue>> _valueFactory;
+    private TValue _value;
+
+    // Constructors
+    public LazyTask(Func<Task<TValue>> valueFactory)
+        => _valueFactory = valueFactory ?? throw new ArgumentNullException(nameof(valueFactory));
+
+    // Properties
+    public bool IsValueInitialized { get; private set; }
+
+    // Methods 
+    /// <summary>
+    /// Gets the lazy initialized value of current <see cref="LazyTask{TValue}"/>.
+    /// Will only initialize only once. Any subsequence call, will retrieve stored value.
+    /// </summary>
+    public async ValueTask<TValue> GetValueAsync()
+    {
+        if (IsValueInitialized)
+            return _value;
+
+        _value = await _valueFactory();
+        IsValueInitialized = true;
+        return _value;
+    }
+}
---
+++ tests/Tests/Common/LazyTaskTests.cs
@@ -0,0 +1,84 @@
+﻿namespace CoreSharp.Common.Tests;
+
+[TestFixture]
+public class LazyTaskTests
+{
+    // Methods
+    [Test]
+    public void Constructor_ValueFactoryIsNull_ThrowArgumentNullException()
+    {
+        // Act 
+        Action action = () => _ = new LazyTask<string>(null);
+
+        // Arrange
+        action.Should().Throw<ArgumentNullException>();
+    }
+
+    [Test]
+    public async Task GetValueAsync_WhenCalled_ReturnsValue()
+    {
+        // Arrange 
+        const string value = "1";
+        static Task<string> GetValueAsync()
+            => Task.FromResult(value);
+        var lazyTask = new LazyTask<string>(GetValueAsync);
+
+        // Act 
+        var lazyValue = await lazyTask.GetValueAsync();
+
+        // Arrange
+        lazyValue.Should().Be(value);
+    }
+
+    [Test]
+    public async Task GetValueAsync_WhenCalledTwice_InitializedOnlyOnce()
+    {
+        // Arrange 
+        var initializeCount = 0;
+        Task<string> GetValueAsync()
+        {
+            initializeCount++;
+            return Task.FromResult<string>(null);
+        }
+
+        var lazyTask = new LazyTask<string>(GetValueAsync);
+
+        // Act 
+        await lazyTask.GetValueAsync();
+        await lazyTask.GetValueAsync();
+
+        // Arrange
+        initializeCount.Should().Be(1);
+    }
+
+    [Test]
+    public void GetValueAsync_BeforeCalled_IsValueInitializedIsFalse()
+    {
+        // Arrange  
+        static Task<string> GetValueAsync()
+            => Task.FromResult<string>(null);
+        var lazyTask = new LazyTask<string>(GetValueAsync);
+
+        // Act 
+        var isValueInitialized = lazyTask.IsValueInitialized;
+
+        // Arrange
+        isValueInitialized.Should().BeFalse();
+    }
+
+    [Test]
+    public async Task GetValueAsync_WhenCalled_SetIsValueInitializedToTrue()
+    {
+        // Arrange  
+        static Task<string> GetValueAsync()
+            => Task.FromResult<string>(null);
+        var lazyTask = new LazyTask<string>(GetValueAsync);
+
+        // Act 
+        await lazyTask.GetValueAsync();
+        var isValueInitialized = lazyTask.IsValueInitialized;
+
+        // Arrange
+        isValueInitialized.Should().BeTrue();
+    }
+}
