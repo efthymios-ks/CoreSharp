ac76eb9638e430c2842db76d5e9c21f1d39140c4
Efthymios <efthymios.ks@gmail.com>
2022-11-29 18:36:54 +0200

    Json, Converters, Added for `CultureInfo`.

---
+++ src/CoreSharp/Json/JsonNet/JsonConverters/CultureInfoJsonConveter.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Globalization;
+
+namespace CoreSharp.Json.JsonNet.JsonConverters;
+
+public sealed class CultureInfoJsonConveter : JsonConverter<CultureInfo>
+{
+    // Methods
+    public override CultureInfo ReadJson(JsonReader reader, Type objectType, CultureInfo existingValue, bool hasExistingValue, JsonSerializer serializer)
+        => reader.Value switch
+        {
+            int cultureCode => CultureInfo.GetCultureInfo(cultureCode),
+            string cultureName => CultureInfo.GetCultureInfo(cultureName),
+            _ => null
+        };
+
+    public override void WriteJson(JsonWriter writer, CultureInfo value, JsonSerializer serializer)
+        => writer.WriteValue(value?.Name);
+}
---
+++ src/CoreSharp/Json/TextJson/JsonConverters/CultureInfoJsonConveter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace CoreSharp.Json.TextJson.JsonConverters;
+
+public sealed class CultureInfoJsonConveter : JsonConverter<CultureInfo>
+{
+    // Methods
+    public override CultureInfo Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        => reader.TokenType switch
+        {
+            JsonTokenType.Number => CultureInfo.GetCultureInfo(reader.GetInt32()),
+            JsonTokenType.String => CultureInfo.GetCultureInfo(reader.GetString()),
+            _ => null
+        };
+
+    public override void Write(Utf8JsonWriter writer, CultureInfo value, JsonSerializerOptions options)
+        => writer.WriteStringValue(value?.Name);
+}
