18acdead6ead58171c0e75a7abedbec67a6be595
Efthymios <efthymios.ks@gmail.com>
2022-12-04 11:00:39 +0200

    Json, ContractResolvers, Required, Exposed functionality to allow combining with other resolvers.

---
+++ src/CoreSharp/Json/JsonNet/ContractResolvers/RequiredContractResolver.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace CoreSharp.Json.JsonNet.ContractResolvers;
+
+public class RequiredContractResolver : DefaultContractResolver
+{
+    // Fields
+    private static RequiredContractResolver _instance;
+
+    // Properties
+    public static RequiredContractResolver Instance
+        => _instance ??= new RequiredContractResolver();
+
+    // Methods
+    protected override JsonObjectContract CreateObjectContract(Type objectType)
+    {
+        var contract = base.CreateObjectContract(objectType);
+        SetRequiredProperties(contract);
+        return contract;
+    }
+
+    public static void SetRequiredProperties(JsonObjectContract contract)
+    {
+        foreach (var jsonProperty in contract.Properties.Where(RequiredPropertyFilter))
+            jsonProperty.Required = Required.Always;
+    }
+
+    private static bool RequiredPropertyFilter(JsonProperty jsonProperty)
+        => jsonProperty.PropertyType.IsValueType
+        && jsonProperty.AttributeProvider.GetAttributes(typeof(RequiredAttribute), inherit: true).Any();
+}
