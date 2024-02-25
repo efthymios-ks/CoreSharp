7aece7ca97aff5a67e97f59acb4807f8665f0e76
Efthymios Koktsidis <efthymios.ks@gmail.com>
2024-02-25 11:36:39 +0200

    feat(Utilities): Add sanitize file name util.

---
+++ src/CoreSharp/Utilities/FileUtils.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CoreSharp.Utilities;
+
+/// <summary>
+/// File utilities.
+/// </summary>
+public static class FileUtils
+{
+    // Fields
+    private static Regex _sanitizeFileNameRegex;
+
+    // Properties
+    private static Regex SanitizeFileNameRegex
+    {
+        get
+        {
+            if (_sanitizeFileNameRegex is null)
+            {
+                var invalidChars = Path.GetInvalidFileNameChars()
+                    .Concat(Path.GetInvalidPathChars())
+                    .ToArray();
+                var invalidCharsAsString = new string(invalidChars);
+                var invalidCharsAsEscapedString = Regex.Escape(invalidCharsAsString);
+                var invalidCharsPattern = $"[{invalidCharsAsEscapedString}]";
+                _sanitizeFileNameRegex = new Regex(invalidCharsPattern, RegexOptions.Compiled);
+            }
+
+            return _sanitizeFileNameRegex;
+        }
+    }
+
+    public static string SanitizeFileName(string fileName)
+    {
+        ArgumentNullException.ThrowIfNull(fileName);
+
+        return SanitizeFileNameRegex.Replace(fileName, "_");
+    }
+}
---
+++ tests/Tests/Utilities/FileUtilsTests.cs
@@ -0,0 +1,32 @@
+﻿namespace CoreSharp.Utilities.Tests;
+
+[TestFixture]
+public class FileUtilsTests
+{
+    [Test]
+    public void SanitizeFilePath_WhenFilePathIsNull_ShouldReturnNull()
+    {
+        // Arrange
+        const string filePath = null;
+
+        // Act
+        Action action = () => FileUtils.SanitizeFileName(filePath);
+
+        // Assert
+        action.Should().ThrowExactly<ArgumentNullException>();
+    }
+
+    [Test]
+    [TestCase("file1.txt", "file1.txt")]
+    [TestCase("file_2.txt", "file_2.txt")]
+    [TestCase("<file>3.txt", "_file_3.txt")]
+    [TestCase("file?4.txt", "file_4.txt")]
+    public void SanitizeFileName_WhenCalled_ShouldReplaceInvalidCharsWithUnderscore(string fileName, string expectedSanitizedFileName)
+    {
+        // Act
+        var sanitizedFileName = FileUtils.SanitizeFileName(fileName);
+
+        // Assert
+        sanitizedFileName.Should().Be(expectedSanitizedFileName);
+    }
+}
